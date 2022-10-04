2d7e463dd129604ca388c5adfc07eb4f18df220c
Sam Witty <samawitty@gmail.com>
2022-10-04 11:47:22 -0400

    Fixed linter to be black compatible

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,6 +2,6 @@
 set -euxo pipefail
 
 mypy --ignore-missing-imports causal_pyro/
-isort --check --diff causal_pyro/ tests/
+isort --check --profile black --diff causal_pyro/ tests/
 black --check causal_pyro/ tests/
 flake8 causal_pyro/ tests/
