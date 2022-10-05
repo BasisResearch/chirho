acc925fb5bfbf6be0c295e0b8dc47c1529686569
Sam Witty <samawitty@gmail.com>
2022-10-05 15:53:15 -0400

    partitioned out test into factual, base counterfactual, and twin world counterfactual

--- tests/test_do_messenger.py
+++ tests/test_do_messenger.py
@@ -19,19 +19,17 @@ logger = logging.getLogger(__name__)
 x_cf_values = [-1.0, 0.0, 2.0, 2]
 
 
-@pytest.mark.parametrize("x_cf_value", x_cf_values)
-def test_counterfactual_handler_smoke(x_cf_value):
+def model():
+    #   z
+    #  /  \
+    # x --> y
+    Z = pyro.sample("z", dist.Normal(0, 1))
+    X = pyro.sample("x", dist.Normal(Z, 1))
+    Y = pyro.sample("y", dist.Normal(0.8 * X + 0.3 * Z, 1))
+    return Z, X, Y
 
-    # estimand: p(y | do(x)) = \int p(y | z, x) p(x' | z) p(z) dz dx'
-    def model():
-        #   z
-        #  /  \
-        # x --> y
-        Z = pyro.sample("z", dist.Normal(0, 1))
-        X = pyro.sample("x", dist.Normal(Z, 1))
-        Y = pyro.sample("y", dist.Normal(0.8 * X + 0.3 * Z, 1))
-        return Z, X, Y
 
+def create_intervened_model(x_cf_value):
     def intervened_model():
         #   z
         #  /  \
@@ -42,106 +40,96 @@ def test_counterfactual_handler_smoke(x_cf_value):
         Y = pyro.sample("y", dist.Normal(0.8 * X + 0.3 * Z, 1))
         return Z, X, Y
 
-    intervened_model_messenger_1 = DoMessenger({"x": torch.tensor(x_cf_value)})(model)
-    intervened_model_messenger_2 = do(model, {"x": torch.tensor(x_cf_value)})
+    return intervened_model
+
+
+def create_intervened_model_1(x_cf_value):
+    return DoMessenger({"x": torch.tensor(x_cf_value)})(model)
+
+
+def create_intervened_model_2(x_cf_value):
+    return do(model, {"x": torch.tensor(x_cf_value)})
+
+
+@pytest.mark.parametrize("x_cf_value", x_cf_values)
+def test_do_messenger_factual(x_cf_value):
+    intervened_model = create_intervened_model(x_cf_value)
+    intervened_model_messenger_1 = create_intervened_model_1(x_cf_value)
+    intervened_model_messenger_2 = create_intervened_model_2(x_cf_value)
 
     with Factual():
-        z_factual, x_factual, y_factual = intervened_model()
-        (
-            z_factual_messenger_1,
-            x_factual_messenger_1,
-            y_factual_messenger_1,
-        ) = intervened_model_messenger_1()
-        (
-            z_factual_messenger_2,
-            x_factual_messenger_2,
-            y_factual_messenger_2,
-        ) = intervened_model_messenger_2()
+        z, x, y = intervened_model()
+        (z_messenger_1, x_messenger_1, y_messenger_1,) = intervened_model_messenger_1()
+        (z_messenger_2, x_messenger_2, y_messenger_2,) = intervened_model_messenger_2()
 
     # assert all xs unique
-    xs_factual = [x_factual, x_factual_messenger_1, x_factual_messenger_2]
-    assert len(xs_factual) == len(set(xs_factual))
+    xs = [x, x_messenger_1, x_messenger_2]
+    assert len(xs) == len(set(xs))
 
     assert (
-        z_factual.shape
-        == x_factual.shape
-        == y_factual.shape
-        == z_factual_messenger_1.shape
-        == x_factual_messenger_1.shape
-        == y_factual_messenger_1.shape
-        == z_factual_messenger_2.shape
-        == x_factual_messenger_2.shape
-        == y_factual_messenger_2.shape
+        z.shape
+        == x.shape
+        == y.shape
+        == z_messenger_1.shape
+        == x_messenger_1.shape
+        == y_messenger_1.shape
+        == z_messenger_2.shape
+        == x_messenger_2.shape
+        == y_messenger_2.shape
         == torch.Size([])
     )
+
+
+@pytest.mark.parametrize("x_cf_value", x_cf_values)
+def test_do_messenger_base_counterfactual(x_cf_value):
+    intervened_model = create_intervened_model(x_cf_value)
+    intervened_model_messenger_1 = create_intervened_model_1(x_cf_value)
+    intervened_model_messenger_2 = create_intervened_model_2(x_cf_value)
 
     with BaseCounterfactual():
-        z_cf, x_cf, y_cf = intervened_model()
+        z, x, y = intervened_model()
 
-        (
-            z_cf_messenger_1,
-            x_cf_messenger_1,
-            y_cf_messenger_1,
-        ) = intervened_model_messenger_1()
+        (z_messenger_1, x_messenger_1, y_messenger_1,) = intervened_model_messenger_1()
 
-        (
-            z_cf_messenger_2,
-            x_cf_messenger_2,
-            y_cf_messenger_2,
-        ) = intervened_model_messenger_2()
+        (z_messenger_2, x_messenger_2, y_messenger_2,) = intervened_model_messenger_2()
 
     # This test should be passing?
-    assert x_cf == x_cf_messenger_1 == x_cf_messenger_2 == x_cf_value
+    assert x == x_messenger_1 == x_messenger_2 == x_cf_value
     assert (
-        z_cf.shape
-        == x_cf.shape
-        == y_cf.shape
-        == z_cf_messenger_1.shape
-        == x_cf_messenger_1.shape
-        == y_cf_messenger_1.shape
-        == z_cf_messenger_2.shape
-        == x_cf_messenger_2.shape
-        == y_cf_messenger_2.shape
+        z.shape
+        == x.shape
+        == y.shape
+        == z_messenger_1.shape
+        == x_messenger_1.shape
+        == y_messenger_1.shape
+        == z_messenger_2.shape
+        == x_messenger_2.shape
+        == y_messenger_2.shape
         == torch.Size([])
     )
+
+
+@pytest.mark.parametrize("x_cf_value", x_cf_values)
+def test_do_messenger_twin_counterfactual(x_cf_value):
+
+    intervened_model = create_intervened_model(x_cf_value)
+    intervened_model_messenger_1 = create_intervened_model_1(x_cf_value)
+    intervened_model_messenger_2 = create_intervened_model_2(x_cf_value)
 
     with TwinWorldCounterfactual(-1):
-        z_cf_twin, x_cf_twin, y_cf_twin = intervened_model()
-        (
-            z_cf_twin_messenger_1,
-            x_cf_twin_messenger_1,
-            y_cf_twin_messenger_1,
-        ) = intervened_model_messenger_1()
-        (
-            z_cf_twin_messenger_2,
-            x_cf_twin_messenger_2,
-            y_cf_twin_messenger_2,
-        ) = intervened_model_messenger_2()
+        z, x, y = intervened_model()
+        (z_messenger_1, x_messenger_1, y_messenger_1,) = intervened_model_messenger_1()
+        (z_messenger_2, x_messenger_2, y_messenger_2,) = intervened_model_messenger_2()
 
+    assert x[0] != x_messenger_1[0] != x_messenger_2[0] != x_cf_value
+    assert x[1] == x_messenger_1[1] == x_messenger_2[1] == x_cf_value
+    assert z.shape == z_messenger_1.shape == z_messenger_2.shape == torch.Size([])
     assert (
-        x_cf_twin[0]
-        != x_cf_twin_messenger_1[0]
-        != x_cf_twin_messenger_2[0]
-        != x_cf_value
-    )
-    assert (
-        x_cf_twin[1]
-        == x_cf_twin_messenger_1[1]
-        == x_cf_twin_messenger_2[1]
-        == x_cf_value
-    )
-    assert (
-        z_cf_twin.shape
-        == z_cf_twin_messenger_1.shape
-        == z_cf_twin_messenger_2.shape
-        == torch.Size([])
-    )
-    assert (
-        x_cf_twin.shape
-        == y_cf_twin.shape
-        == x_cf_twin_messenger_1.shape
-        == y_cf_twin_messenger_1.shape
-        == x_cf_twin_messenger_2.shape
-        == y_cf_twin_messenger_2.shape
+        x.shape
+        == y.shape
+        == x_messenger_1.shape
+        == y_messenger_1.shape
+        == x_messenger_2.shape
+        == y_messenger_2.shape
         == (2,)
     )
