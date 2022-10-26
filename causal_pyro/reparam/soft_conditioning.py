7cf1a4dc384e63e1b3d5a5c8bca72ae23ebe25a3
Eli <eli@basis.ai>
2022-10-26 13:00:30 -0400

    use deterministic

--- causal_pyro/reparam/soft_conditioning.py
+++ causal_pyro/reparam/soft_conditioning.py
@@ -9,7 +9,7 @@ from .dispatched_strategy import DispatchedStrategy
 class AutoSoftConditioning(DispatchedStrategy):
     def __init__(self, kernel: pyro.contrib.gp.kernels.Kernel, alpha: float = 1.0):
         self.kernel = kernel
-        self.alpha = torch.as_tensor(alpha)
+        self.alpha = alpha
         super().__init__()
 
 
@@ -27,4 +27,4 @@ def _auto_soft_conditioning_delta(
         approx_log_prob = torch.where(fn.v == value, self.alpha, 1 - self.alpha)
 
     pyro.factor("approx_log_prob", approx_log_prob)
-    return pyro.distributions.Delta(v=value, event_dim=fn.event_dim), value, True
+    return self.deterministic(value, fn.event_dim), value, True
