4119cac6bc90827f4039e2993ab85bc6dedcaa67
Zenna Tavares <zennatavares@gmail.com>
2022-11-24 06:02:05 -0500

    add some plots

--- sandbox/mediation.py
+++ sandbox/mediation.py
@@ -16,7 +16,8 @@ from causal_pyro.query.do_messenger import do
 from pyro.infer import SVI, Trace_ELBO
 from pyro.infer.autoguide import AutoDiagonalNormal
 
-## Queries 
+## Queries
+
 def direct_effect(model, X, x, x_prime, Z, dim = -1) -> Callable:
     "# natural direct effect: DE{x,x'}(Y) = E[ Y(X=x', Z(X=x)) - E[Y(X=x)] ]"
     return do(actions={X: x})(
@@ -26,32 +27,35 @@ def direct_effect(model, X, x, x_prime, Z, dim = -1) -> Callable:
                   model))))
 
 def direct_effect_manual(model, X, x, x_prime, Z, dim = -1) -> Callable:
-    "# natural direct effect: DE{x,x'}(Y) = E[ Y(X=x', Z(X=x)) - E[Y(X=x)] ]"
-    # Sample value of intermediate variable Z in model from trace
-    def odd_model():
-      # t = pyro.poutine.trace(health_model).get_trace()
+  "# natural direct effect: DE{x,x'}(Y) = E[ Y(X=x', Z(X=x)) - E[Y(X=x)] ]"
+  # Sample value of intermediate variable Z in model from trace
+  def odd_model():
+    # t = pyro.poutine.trace(health_model).get_trace()
 
-      # Value of z in intervention mode where X = x_prime
-      m1 = do(actions={X: x_prime})(model)
-      t = pyro.poutine.trace(m1).get_trace()
-      z_ = t.nodes[Z]["value"]
-      print("z_: ", z_) 
-      return do(actions={X: x})(
-        do(actions={X: x_prime})(
-            do(actions={Z: z_})(
-                MultiWorldCounterfactual(-1)(
-                  model))))()
-    return odd_model
+    # Value of z in intervention mode where X = x_prime
+    m1 = do(actions={X: x_prime})(model)
+    t = pyro.poutine.trace(m1).get_trace()
+    z_ = t.nodes[Z]["value"]
+    print("z_: ", z_) 
+    return do(actions={X: x})(
+      do(actions={X: x_prime})(
+          do(actions={Z: z_})(
+              MultiWorldCounterfactual(-1)(
+                model))))()
+  return odd_model
 
 def average_natural_direct_effect(model, X, x, x_prime, Z, dim = -1, nsamples = 1000):
   # Let's draw nsamples using a plate
-  def draw_samples(model, nsamples):
-    with pyro.plate("data", nsamples, dim = -2) as ind:
-      return model()
-  
+  de_model = direct_effect_manual(model, X, x, x_prime, Z, dim)
+  # First let's get samples for the forward model
+  with pyro.plate("samples", nsamples, dim = -3) as ind:
+    samples = de_model()
+  return samples
 
 ## Toy Example 
+
 def health_model():
+  print("FIXME: Remove modifications to main causal pyro, printlns")
   taken_med = pyro.sample("taken_med", dist.Bernoulli(0.5))
   print("taken_med: ", taken_med)
   nsleep_mean = torch.where(taken_med == 1, torch.tensor(8.), torch.tensor(6.))
@@ -68,11 +72,46 @@ def health_model():
   well_being = pyro.deterministic("well_being", health * med_effective, event_dim=0)
   return well_being
 
-def test_health_model():
-  de_toy = direct_effect_manual(health_model, "taken_med", torch.tensor(1), torch.tensor(0), "nsleep")
-  return de_toy
+### Plotting
 
-test_health_model()()
+# Plot some samples from health_model (intermediate variables)
+# First lett's get samples for the forward model
+def get_samples(model, nsamples):
+  # This won't give us intermediate variables, so instead:
+  with pyro.plate("data", nsamples, dim = -2) as ind:
+    return pyro.poutine.trace(model).get_trace()
+
+# Import seaborn
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Now let's plot distributions for all variables (they are either Booleans or continous)
+def plot_distributions():
+  samples = get_samples(health_model, 1000)
+  # plot histogram taken_mod in seaborn (use histplot or displot)
+  sns.histplot(samples.nodes["taken_med"]["value"])
+  plt.show()
+  sns.histplot(samples.nodes["nsleep"]["value"])
+  plt.show()
+  sns.histplot(samples.nodes["rested"]["value"])
+  plt.show()
+  sns.histplot(samples.nodes["med_effective"]["value"])
+  plt.show()
+  sns.histplot(samples.nodes["health"]["value"])
+  plt.show()
+  sns.histplot(samples.nodes["well_being"]["value"])
+  plt.show()
+  
+def test_health_model():
+  # de_toy = direct_effect_manual(health_model, "taken_med", torch.tensor(1), torch.tensor(0), "nsleep")
+  return average_natural_direct_effect(health_model, "taken_med", torch.tensor(1), torch.tensor(0), "nsleep")
+  # return de_toy
+
+test_health_model()
+
+## Exmaple: Substance Abuse
+
+### Data Loading
 
 # The csv data is stored at http://data.mxnet.io/data/personality.csv
 # Let's load the data from the url
@@ -98,8 +137,7 @@ keys_ = data.keys()
 # Now let's get a key for first column
 key_ = keys_[0]
 
-
-# Variables
+### The Model
 
 # dev_peer represents engagement with deviant peer groups and it was coded as “0: No” and “1: Yes”;
 # sub_exp represents experimentation with drugs and it was coded as “0: No” and “1: Yes”;
@@ -107,13 +145,12 @@ key_ = keys_[0]
 # sub_disorder represents diagnosis of substance use disorder in young adulthood and it was coded as “0: No: and “1: Yes”.
 # conflict represents level of family conflict. It will be used as a covariate in this analysis.
 
+### Question: What is the direct effect of deviant peer engagement on substance use disorder?
+
 # Does reduced engagement with deviant peer groups and reduced experimentation with drugs mediate the effect of the family intervention during adolescence on future substance use disorder?
 # In the example, we examine the causal chain “Participation in family intervention -> reduced engagement with deviant peer groups and experimentation with drugs -> substance use disorder in young adulthood”.
 # In other words, we expect that participation in family intervention during adolescence reduces engagement with deviant peer groups and experimentation with drugs, which in turns reduces the likelihood for substance use disorder in young adulthood.
 
-# Let's plot the distribution over the field Agreeableness
-
-# Let's do logistic regression in Pyro
 # Assumine that 
 class LogisticRegression(torch.nn.Module):
     def __init__(self, D_in, D_out):
@@ -192,8 +229,16 @@ def execute_model():
 def p_t(row):
   return torch.tensor(row.values).reshape(-1, 1).float()
 
-def conditioned_model():
-  return pyro.condition(execute_model, data={"fam_int": p_t(ddata["fam_int"])})
+# def conditioned_model():
+#   return pyro.condition(execute_model, data={"fam_int": p_t(ddata["fam_int"])})
+
+
+m = CausalModel()
+cm = pyro.condition(m, data={"fam_int": p_t(ddata["fam_int"]), \
+                              "dev_peer": p_t(ddata["dev_peer"]), \
+                              "sub_exp": p_t(ddata["sub_exp"]), \
+                              "sub_disorder": p_t(ddata["sub_disorder"])})
+
 
 # Inference
 def train(cm, *args):
@@ -210,15 +255,7 @@ def train(cm, *args):
   l = svi.step(*args)
   return l
 
-m = CausalModel()
-cm = pyro.condition(m, data={"fam_int": p_t(ddata["fam_int"]), \
-                              "dev_peer": p_t(ddata["dev_peer"]), \
-                              "sub_exp": p_t(ddata["sub_exp"]), \
-                              "sub_disorder": p_t(ddata["sub_disorder"])})
-
-
 result = train(cm)
-
 
 def test():
   return direct_effect(m, "fam_int", torch.zeros(1), torch.zeros(1), "dev_peer")
