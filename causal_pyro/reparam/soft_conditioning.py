ad9081531225bd98ec385d5d53943ba541a57e39
Eli <eli@basis.ai>
2022-12-24 17:15:43 -0500

    Add a reparam for deep scm

--- causal_pyro/reparam/soft_conditioning.py
+++ causal_pyro/reparam/soft_conditioning.py
@@ -1,9 +1,44 @@
-from typing import Optional
+from typing import Optional, List, Union
+
+import torch
 
 import pyro
 from pyro.contrib.gp.kernels import Kernel
-from pyro.distributions import Delta, MaskedDistribution
+from pyro.distributions import Delta, MaskedDistribution, TransformedDistribution, transforms
 from pyro.infer.reparam.reparam import Reparam
+
+
+class ConditionalComposeTransformModule(transforms.ConditionalTransformModule):
+    def __init__(self, transforms: List[Union[transforms.TransformModule, transforms.ConditionalTransformModule]]):
+        self.transforms = [
+            transforms.ConstantConditionalTransform(t)
+            if not isinstance(t, transforms.ConditionalTransform)
+            else t
+            for t in transforms
+        ]
+        super().__init__(event_dim=transforms[0].event_dim)
+
+    def condition(self, context: torch.Tensor):
+        return transforms.ComposeTransformModule([t.condition(context) for t in self.transforms])
+
+
+class TransformInferReparam(Reparam):
+
+    def apply(self, msg):
+        fn = msg["fn"]
+        value = msg["value"]
+        is_observed = msg["is_observed"]
+
+        assert isinstance(fn, TransformedDistribution)
+        assert value is not None
+
+        t = fn.transforms[0] if len(fn.transforms) == 1 else \
+            transforms.ComposeTransform(fn.transforms)
+
+        obs_base_dist = Delta(value, event_dim=fn.event_dim).expand(fn.batch_shape).mask(False)
+        latent_base_dist = TransformedDistribution(obs_base_dist, [t.inv])
+        new_obs_dist = TransformedDistribution(latent_base_dist, [t])
+        return {"fn": new_obs_dist, "value": value, "is_observed": is_observed}
 
 
 class KernelABCReparam(Reparam):
@@ -44,13 +79,20 @@ class AutoSoftConditioning(pyro.infer.reparam.strategies.Strategy):
             and msg["is_observed"]
             and isinstance(msg["fn"], MaskedDistribution)
             and isinstance(msg["fn"].base_dist, Delta)
-            and msg["infer"].get("is_deteministic", False)
+            and msg["fn"]._mask is False
+            and msg["infer"].get("is_deterministic", False)
         )
 
     def configure(self, msg: dict) -> Optional[Reparam]:
-        if not self._is_deterministic(msg):
+        if not msg["is_observed"]:
             return None
 
-        return KernelABCReparam(
-            kernel=pyro.contrib.gp.kernels.RBF(input_dim=msg["fn"].event_dim)
-        )
+        if isinstance(msg["fn"], TransformedDistribution):
+            return TransformInferReparam()
+
+        if self._is_deterministic(msg):
+            return KernelABCReparam(
+                kernel=pyro.contrib.gp.kernels.RBF(input_dim=msg["fn"].event_dim)
+            )
+
+        return None
