20a23245f748d596cb341c0ce47e415c14347249
Eli <eli@basis.ai>
2023-01-19 13:05:20 -0500

    remove cf tests from test_worlds

--- tests/test_worlds.py
+++ tests/test_worlds.py
@@ -5,13 +5,9 @@ import pyro.distributions as dist
 import pytest
 import torch
 
-from causal_pyro.counterfactual.handlers import MultiWorldCounterfactual
-from causal_pyro.primitives import intervene
-
-from causal_pyro.counterfactual.index_set import IndexSet, scatter, gather
-from causal_pyro.counterfactual.worlds import (
-    MultiWorld, get_mask, get_value_world, add_world_plates, get_world_plates,
-)
+from causal_pyro.counterfactual.index_set import IndexSet, indices_of, scatter, gather
+from causal_pyro.counterfactual.worlds import IndexPlatesMessenger, \
+    indexset_as_mask, mask_as_indexset, add_indices, get_full_index, get_index_plates
 
 
 logger = logging.getLogger(__name__)
@@ -49,21 +45,21 @@ EVENT_SHAPES = list(set([
 @pytest.mark.parametrize("event_shape", EVENT_SHAPES, ids=str)
 @pytest.mark.parametrize("num_named", [0, 1, 2, 3])
 @pytest.mark.parametrize("first_available_dim", [-1, -2, -3])
-def test_get_world_tensor(batch_shape, event_shape, num_named, first_available_dim):
+def test_indices_of_tensor(batch_shape, event_shape, num_named, first_available_dim):
     batch_dim_names = {
         first_available_dim - i: f"b{i}"
         for i in range(min(num_named, len(batch_shape)))
         if len(batch_shape) >= i - first_available_dim
     }
 
-    with MultiWorld(first_available_dim):
+    with IndexPlatesMessenger(first_available_dim):
         # overapproximate
-        add_world_plates(IndexSet(**{
+        add_indices(IndexSet(**{
             name: set(range(max(2, batch_shape[dim])))
             for dim, name in batch_dim_names.items()
         }))
         value = torch.randn(batch_shape + event_shape)
-        actual_world = get_value_world(value, event_dim=len(event_shape))
+        actual_world = indices_of(value, event_dim=len(event_shape))
 
     expected_world = IndexSet(**{
         name: set(range(batch_shape[dim]))
@@ -78,21 +74,21 @@ def test_get_world_tensor(batch_shape, event_shape, num_named, first_available_d
 @pytest.mark.parametrize("event_shape", EVENT_SHAPES, ids=str)
 @pytest.mark.parametrize("num_named", [0, 1, 2, 3])
 @pytest.mark.parametrize("first_available_dim", [-1, -2, -3])
-def test_get_world_distribution(batch_shape, event_shape, num_named, first_available_dim):
+def test_indices_of_distribution(batch_shape, event_shape, num_named, first_available_dim):
     batch_dim_names = {
         first_available_dim - i: f"b{i}"
         for i in range(min(num_named, len(batch_shape)))
         if len(batch_shape) >= i - first_available_dim
     }
 
-    with MultiWorld(first_available_dim):
+    with IndexPlatesMessenger(first_available_dim):
         # overapproximate
-        add_world_plates(IndexSet(**{
+        add_indices(IndexSet(**{
             name: set(range(max(2, batch_shape[dim])))
             for dim, name in batch_dim_names.items()
         }))
         value = dist.Normal(0, 1).expand(batch_shape + event_shape).to_event(len(event_shape))
-        actual_world = get_value_world(value, event_dim=len(event_shape))
+        actual_world = indices_of(value, event_dim=len(event_shape))
 
     expected_world = IndexSet(**{
         name: set(range(batch_shape[dim]))
@@ -105,9 +101,9 @@ def test_get_world_distribution(batch_shape, event_shape, num_named, first_avail
 
 @pytest.mark.parametrize("batch_shape", BATCH_SHAPES)
 @pytest.mark.parametrize("event_shape", EVENT_SHAPES)
-def test_get_mask(batch_shape, event_shape):
+def test_indexset_as_mask(batch_shape, event_shape):
     world = IndexSet(X={0, 1}, Y={0, 1}, Z={0, 1})
-    mask = get_mask(world, event_dim=len(event_shape))
+    mask = indexset_as_mask(world, event_dim=len(event_shape))
     assert mask.shape == (2, 2, 2)
     assert mask[0, 1, :].all()
     assert mask[1, 1, :].all()
@@ -143,62 +139,3 @@ def test_scatter_gather_tensor(batch_shape, event_shape):
     world = IndexSet(...)
     gather(scatter(value, world, event_dim=len(event_shape)), world, event_dim=len(event_shape))
     assert (orig_value == value).all()
-
-
-@pytest.mark.xfail(reason="not yet implemented")
-@pytest.mark.parametrize("x_cf_value", [-1.0, 0.0, 2.0, 2])
-@pytest.mark.parametrize("cf_dim", [-1, -2, -3])
-@pytest.mark.parametrize("impl", [MultiWorldCounterfactual, MultiWorldInterventions])
-def test_multiple_interventions(x_cf_value, cf_dim, impl):
-    def model():
-        #   z
-        #  /  \
-        # x --> y
-        Z = pyro.sample("z", dist.Normal(0, 1))
-        Z = intervene(Z, torch.tensor(x_cf_value - 1.0))
-        X = pyro.sample("x", dist.Normal(Z, 1))
-        X = intervene(X, torch.tensor(x_cf_value))
-        Y = pyro.sample("y", dist.Normal(0.8 * X + 0.3 * Z, 1))
-        return Z, X, Y
-
-    with impl(cf_dim):
-        Z, X, Y = model()
-
-    assert Z.shape == (2,)
-    assert X.shape == (2, 2)
-    assert Y.shape == (2, 2)
-
-
-@pytest.mark.xfail(reason="not yet implemented")
-@pytest.mark.parametrize("x_cf_value", [-1.0, 0.0, 2.0, 2])
-@pytest.mark.parametrize("event_shape", [(), (4,), (4, 3)])
-@pytest.mark.parametrize("cf_dim", [-1, -2, -3])
-@pytest.mark.parametrize("impl", [MultiWorldCounterfactual, MultiWorldInterventions])
-def test_multiple_interventions_unnecessary_nesting(x_cf_value, event_shape, cf_dim, impl):
-    def model():
-        #   z
-        #     \
-        # x --> y
-        Z = pyro.sample(
-            "z", dist.Normal(0, 1).expand(event_shape).to_event(len(event_shape))
-        )
-        Z = intervene(
-            Z, torch.full(event_shape, x_cf_value - 1.0), event_dim=len(event_shape)
-        )
-        X = pyro.sample(
-            "x", dist.Normal(0, 1).expand(event_shape).to_event(len(event_shape))
-        )
-        X = intervene(
-            X, torch.full(event_shape, x_cf_value), event_dim=len(event_shape)
-        )
-        Y = pyro.sample(
-            "y", dist.Normal(0.8 * X + 0.3 * Z, 1).to_event(len(event_shape))
-        )
-        return Z, X, Y
-
-    with impl(cf_dim):
-        Z, X, Y = model()
-
-    assert Z.shape == (2,) + (1,) * (-cf_dim - 1) + event_shape
-    assert X.shape == (2, 1) + (1,) * (-cf_dim - 1) + event_shape
-    assert Y.shape == (2, 2) + (1,) * (-cf_dim - 1) + event_shape
