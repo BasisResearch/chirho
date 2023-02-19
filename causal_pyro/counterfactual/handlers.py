27fb54c3c72ac3ce466b20ce85fd4b579c0d8a01
Eli <eli@basis.ai>
2023-02-19 08:34:00 -0500

    rename factual_conditioning -> conditioning

--- causal_pyro/counterfactual/handlers.py
+++ causal_pyro/counterfactual/handlers.py
@@ -1,42 +1,33 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Union
 
 import pyro
 
+from causal_pyro.counterfactual.conditioning import (
+    AmbiguousConditioningReparam,
+    AmbiguousConditioningStrategy,
+    AutoFactualConditioning,
+)
 from causal_pyro.counterfactual.internals import IndexPlatesMessenger, add_indices
-from causal_pyro.counterfactual.selection import SelectCounterfactual, SelectFactual
-from causal_pyro.primitives import IndexSet, indices_of, join, merge
+from causal_pyro.primitives import IndexSet, join, merge
 
 
-class BaseCounterfactual(pyro.poutine.messenger.Messenger):
+CondStrategy = Union[
+    Dict[str, AmbiguousConditioningReparam],
+    AmbiguousConditioningStrategy
+]
+
+
+class BaseCounterfactual(pyro.poutine.reparam_messenger.ReparamMessenger):
     """
     Base class for counterfactual handlers.
     """
+    def __init__(self, config: Optional[CondStrategy] = None):
+        if config is None:
+            config = AutoFactualConditioning()
+        super().__init__(config=config)
 
     def _pyro_intervene(self, msg: Dict[str, Any]) -> None:
         msg["stop"] = True
-
-    def _pyro_sample(self, msg: Dict[str, Any]) -> None:
-        if (
-            not msg["is_observed"]
-            or msg["value"] is None
-            or pyro.poutine.util.site_is_subsample(msg)
-            or msg["infer"].get("_cf_conditioned", False)  # don't apply recursively
-            or not indices_of(msg["fn"])
-        ):
-            return None
-
-        with pyro.poutine.infer_config(config_fn=lambda msg: {"_cf_conditioned": True}):
-            with SelectFactual() as fw:
-                fv = pyro.sample(msg["name"] + "_factual", msg["fn"], obs=msg["value"])
-
-            with SelectCounterfactual() as cw:
-                cv = pyro.sample(msg["name"] + "_counterfactual", msg["fn"])
-
-        event_dim = len(msg["fn"].event_shape)
-        msg["value"] = merge({fw.indices: fv, cw.indices: cv}, event_dim=event_dim)
-        msg["fn"] = pyro.distributions.Delta(msg["value"], event_dim=event_dim).mask(
-            False
-        )
 
 
 class Factual(BaseCounterfactual):
