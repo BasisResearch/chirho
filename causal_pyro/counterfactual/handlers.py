4b39840e4d6025c3cc577b7da2ec712ad6694e8c
Eli <eli@basis.ai>
2023-02-19 08:15:12 -0500

    split selection into separate file

--- causal_pyro/counterfactual/handlers.py
+++ causal_pyro/counterfactual/handlers.py
@@ -2,12 +2,8 @@ from typing import Any, Dict
 
 import pyro
 
-from causal_pyro.counterfactual.internals import (
-    IndexPlatesMessenger,
-    add_indices,
-    get_index_plates,
-    indexset_as_mask,
-)
+from causal_pyro.counterfactual.internals import IndexPlatesMessenger, add_indices
+from causal_pyro.counterfactual.selection import SelectCounterfactual, SelectFactual
 from causal_pyro.primitives import IndexSet, indices_of, join, merge
 
 
@@ -82,39 +78,3 @@ class TwinWorldCounterfactual(IndexPlatesMessenger, BaseCounterfactual):
         add_indices(join(obs_indices, act_indices))
 
         msg["value"] = merge({obs_indices: obs, act_indices: act}, event_dim=event_dim)
-
-
-class IndexSetMaskMessenger(pyro.poutine.messenger.Messenger):
-    """
-    Effect handler to select a subset of worlds.
-    """
-
-    @property
-    def indices(self) -> IndexSet:
-        raise NotImplementedError
-
-    def _pyro_sample(self, msg: Dict[str, Any]) -> None:
-        mask = indexset_as_mask(self.indices)
-        msg["mask"] = mask if msg["mask"] is None else msg["mask"] & mask
-
-
-class SelectCounterfactual(IndexSetMaskMessenger):
-    """
-    Effect handler to select only counterfactual worlds.
-    """
-
-    @property
-    def indices(self) -> IndexSet:
-        return IndexSet(
-            **{f.name: set(range(1, f.size)) for f in get_index_plates().values()}
-        )
-
-
-class SelectFactual(IndexSetMaskMessenger):
-    """
-    Effect handler to select only factual world.
-    """
-
-    @property
-    def indices(self) -> IndexSet:
-        return IndexSet(**{f.name: {0} for f in get_index_plates().values()})
