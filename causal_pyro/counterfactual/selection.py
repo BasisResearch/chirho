9fca015a01907e8a090ee30e4c554ef98a5fcd0d
Eli <eli@elibingham.com>
2023-03-08 16:29:40 -0500

    functions

--- causal_pyro/counterfactual/selection.py
+++ causal_pyro/counterfactual/selection.py
@@ -15,14 +15,22 @@ class IndexSetMaskMessenger(pyro.poutine.messenger.Messenger):
     Effect handler to select a subset of worlds.
     """
 
-    @property
-    def indices(self) -> IndexSet:
-        raise NotImplementedError
+    indices: IndexSet
 
     def _pyro_sample(self, msg: Dict[str, Any]) -> None:
         mask_device = get_sample_msg_device(msg["fn"], msg["value"])
         mask = indexset_as_mask(self.indices, device=mask_device)
         msg["mask"] = mask if msg["mask"] is None else msg["mask"] & mask
+
+
+def factual_indices() -> IndexSet:
+    return IndexSet(**{f.name: {0} for f in get_index_plates().values()})
+
+
+def counterfactual_indices() -> IndexSet:
+    return IndexSet(
+        **{f.name: set(range(1, f.size)) for f in get_index_plates().values()}
+    )
 
 
 class SelectCounterfactual(IndexSetMaskMessenger):
@@ -32,9 +40,7 @@ class SelectCounterfactual(IndexSetMaskMessenger):
 
     @property
     def indices(self) -> IndexSet:
-        return IndexSet(
-            **{f.name: set(range(1, f.size)) for f in get_index_plates().values()}
-        )
+        return counterfactual_indices()
 
 
 class SelectFactual(IndexSetMaskMessenger):
@@ -44,4 +50,4 @@ class SelectFactual(IndexSetMaskMessenger):
 
     @property
     def indices(self) -> IndexSet:
-        return IndexSet(**{f.name: {0} for f in get_index_plates().values()})
+        return factual_indices()
