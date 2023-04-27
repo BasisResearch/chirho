59bd6a1a743a4cde3a14d315af1dfee4a7a98173
Eli <eli@elibingham.com>
2023-04-27 12:23:29 +0100

    forgot file

---
+++ causal_pyro/effectful/ops/interpretations.py
@@ -0,0 +1,37 @@
+from typing import Generic, List, Optional, TypeVar
+
+from .terms import Kind, Operation, Term, Context, Form, define, get_name, get_head, get_args, read
+
+
+S, T = TypeVar("S"), TypeVar("T")
+
+
+@define(Kind)
+class Interpretation(Generic[T]):
+    pass
+
+# Model ?= Context[Term[T]]
+
+@define(Operation)
+def get_model(model: Interpretation[T], op: Operation[T]) -> Term[T]:
+    return read(model, get_name(op))
+
+
+@define(Operation)
+def union(model: Interpretation[T], other: Interpretation[T]) -> Interpretation[T]:
+    ...
+
+
+@define(Operation)
+def compose(model: Interpretation[T], other: Interpretation[T]) -> Interpretation[T]:
+    ...
+
+
+@define(Operation)
+def product(model: Interpretation[T], comodel: Interpretation[T]) -> Interpretation[T]:
+    ...
+
+
+@define(Operation)
+def quotient(model: Interpretation[T], other: Interpretation[T]) -> Interpretation[T]:
+    ...
