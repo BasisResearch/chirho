edeeabe70c61ad2b8ceda508f02bdb5c60a0da7a
Eli <eli@elibingham.com>
2023-06-09 08:22:47 -0400

    fix foldeddistribution handling

--- tests/counterfactual/test_ambiguity.py
+++ tests/counterfactual/test_ambiguity.py
@@ -24,7 +24,8 @@ logger = logging.getLogger(__name__)
 )
 @pytest.mark.parametrize("cf_dim", [-1, -2, -3])
 @pytest.mark.parametrize("event_shape", [(), (4,), (4, 3)])
-def test_ambiguous_conditioning_transform(cf_class, cf_dim, event_shape):
+@pytest.mark.parametrize("x_folded", [True, False])
+def test_ambiguous_conditioning_transform(cf_class, cf_dim, event_shape, x_folded):
     event_dim = len(event_shape)
 
     def model():
@@ -32,13 +33,15 @@ def test_ambiguous_conditioning_transform(cf_class, cf_dim, event_shape):
         #  /  \
         # v    v
         # y --> z
-        X = pyro.sample(
-            "x",
-            dist.TransformedDistribution(
-                dist.Normal(0.0, 1).expand(event_shape).to_event(event_dim),
-                [dist.transforms.ExpTransform()],
-            ),
-        )
+        X_base_dist = dist.Normal(0.0, 1)
+        if x_folded:
+            X_dist = dist.FoldedDistribution(X_base_dist).expand(event_shape).to_event(event_dim)
+        else:
+            X_dist = dist.TransformedDistribution(
+                X_base_dist.expand(event_shape).to_event(event_dim),
+                [dist.transforms.ExpTransform()]
+            )
+        X = pyro.sample("x", X_dist)
         Y = pyro.sample(
             "y",
             dist.TransformedDistribution(
