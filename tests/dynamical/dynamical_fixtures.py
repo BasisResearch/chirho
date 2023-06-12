7a55b98f691bfb7d20a6da7b1f2a6031c5f22460
Raj Agrawal <r.agrawal.mit@gmail.com>
2023-06-12 11:27:48 -0400

    tests for point observations

--- tests/dynamical/dynamical_fixtures.py
+++ tests/dynamical/dynamical_fixtures.py
@@ -37,47 +37,61 @@ class SimpleSIRDynamics(ODEDynamics):
         dX.I = self.beta * X.S * X.I - self.gamma * X.I
         dX.R = self.gamma * X.I
 
+    def observation(self, X: State[torch.Tensor]):
+        S_obs = pyro.sample("S_obs", Normal(X.S, 1))
+        I_obs = pyro.sample("I_obs", Normal(X.I, 1))
+        R_obs = pyro.sample("R_obs", Normal(X.R, 1))
+        usa_expected_cost = torch.relu(S_obs + 2 * I_obs - R_obs)
+        pyro.sample("usa_cost", Normal(usa_expected_cost, 1))
+
 
 @pytest.fixture
 def sir_ode():
     return SimpleSIRDynamics()
 
 
-def check_trajectory_keys_match(traj1: Trajectory[torch.tensor], traj2: Trajectory[torch.tensor]):
+def check_trajectory_keys_match(
+    traj1: Trajectory[torch.tensor], traj2: Trajectory[torch.tensor]
+):
     assert traj2.keys == traj1.keys, "Trajectories have different state variables."
     return True
 
 
-def check_trajectory_length_match(traj1: Trajectory[torch.tensor], traj2: Trajectory[torch.tensor]):
+def check_trajectory_length_match(
+    traj1: Trajectory[torch.tensor], traj2: Trajectory[torch.tensor]
+):
     for k in traj1.keys:
-        assert len(getattr(traj2, k)) == len(getattr(traj1, k)),\
-            f"Trajectories have different lengths for variable {k}."
+        assert len(getattr(traj2, k)) == len(
+            getattr(traj1, k)
+        ), f"Trajectories have different lengths for variable {k}."
     return True
 
 
 def check_trajectories_match(
-        traj1: Trajectory[torch.tensor], traj2: Trajectory[torch.tensor]):
-
+    traj1: Trajectory[torch.tensor], traj2: Trajectory[torch.tensor]
+):
     assert check_trajectory_keys_match(traj1, traj2)
 
     assert check_trajectory_length_match(traj1, traj2)
 
     for k in traj1.keys:
-        assert torch.allclose(getattr(traj2, k), getattr(traj1, k)),\
-            f"Trajectories differ in state trajectory of variable {k}, but should be identical."
+        assert torch.allclose(
+            getattr(traj2, k), getattr(traj1, k)
+        ), f"Trajectories differ in state trajectory of variable {k}, but should be identical."
 
     return True
 
 
 def check_trajectories_match_in_all_but_values(
-        traj1: Trajectory[torch.tensor], traj2: Trajectory[torch.tensor]):
-
+    traj1: Trajectory[torch.tensor], traj2: Trajectory[torch.tensor]
+):
     assert check_trajectory_keys_match(traj1, traj2)
 
     assert check_trajectory_length_match(traj1, traj2)
 
     for k in traj1.keys:
-        assert not torch.allclose(getattr(traj2, k), getattr(traj1, k)),\
-            f"Trajectories are identical in state trajectory of variable {k}, but should differ."
+        assert not torch.allclose(
+            getattr(traj2, k), getattr(traj1, k)
+        ), f"Trajectories are identical in state trajectory of variable {k}, but should differ."
 
     return True
--- tests/dynamical/test_static_observation.py
+++ tests/dynamical/test_static_observation.py
@@ -5,86 +5,64 @@ import pyro
 import pytest
 import torch
 
-from pyro.distributions import Normal, Uniform
-
-
-import pyro
-import torch
-from pyro.distributions import constraints
-
-from causal_pyro.dynamical.ops import State, simulate, Trajectory
+from causal_pyro.dynamical.ops import State
 from causal_pyro.dynamical.handlers import (
-    ODEDynamics,
-    PointInterruption,
     PointObservation,
     SimulatorEventLoop,
     simulate,
 )
 
-from .dynamical_fixtures import (
-    sir_ode,
-    check_trajectories_match,
-    check_trajectories_match_in_all_but_values,
-)
+from .dynamical_fixtures import sir_ode
 
 logger = logging.getLogger(__name__)
 
-# Points at which to measure the state of the system.
-tspan_values = torch.tensor([1.0, 2.0, 3.0])
-
-# Initial state of the system.
-init_state_values = State(
-    S=torch.tensor(10.0), I=torch.tensor(3.0), R=torch.tensor(1.0)
-)
-
-obs_state = State(S=torch.tensor(5), I=torch.tensor(4), R=torch.tensor(5))
-
-loglikelihood = lambda state: Normal(state.S, 1).log_prob(obs_state.S)
-
-# Define intervention times before all tspan values.
-condition_times = [2.0]
-
-eps = 1e-3
+# Global variables for tests
+init_state = State(S=torch.tensor(1.0), I=torch.tensor(2.0), R=torch.tensor(3.3))
+tspan = torch.tensor([1.0, 2.0, 3.0, 4.0])
 
 
-@pytest.mark.parametrize("init_state", [init_state_values])
-@pytest.mark.parametrize("tspan", [tspan_values])
-@pytest.mark.parametrize("condition_time", condition_times)
-def test_point_intervention_causes_difference(
-    sir_ode, init_state, tspan, condition_time
-):
-    observational_execution_result = simulate(sir_ode, init_state, tspan)
-
-    # Simulate with the intervention and ensure that the result differs from the observational execution.
-    with SimulatorEventLoop():
-        with PointObservation(time=condition_time, loglikelihood=loglikelihood):
-            # if condition_time < tspan[0]:
-            #     with pytest.raises(
-            #         ValueError, match="is before the first time in the timespan"
-            #     ):
-            #         simulate(sir_ode, init_state, tspan)
-            #     return
-            # else:
-            result_single_pint = simulate(sir_ode, init_state, tspan)
-
-    assert check_trajectories_match_in_all_but_values(
-        observational_execution_result, result_single_pint
-    )
-
-
-# Check that there is a log factor at the observed site
-def test_log_factor_exists():
-    init_state = State(S=torch.tensor(1.0), I=torch.tensor(2.0), R=torch.tensor(3.3))
-    tspan = torch.tensor([1.0, 2.0, 3.0, 4.0])
-
-    new_state = State(S=torch.tensor(10.0))
+def test_multiple_point_observations(sir_ode):
+    """
+    Tests if multiple PointObservation handlers can be composed.
+    """
     S_obs = torch.tensor(10.0)
-    loglikelihood = lambda state: Normal(state.S, 1).log_prob(S_obs)
+    data1 = {"S_obs": S_obs}
+    data2 = {"I_obs": torch.tensor(5.0), "R_obs": torch.tensor(5.0)}
+    with SimulatorEventLoop():
+        with PointObservation(time=3.1, data=data2):
+            with PointObservation(time=2.9, data=data1):
+                result = simulate(sir_ode, init_state, tspan)
 
+    assert result.S.shape[0] == 4
+    assert result.I.shape[0] == 4
+    assert result.R.shape[0] == 4
+
+
+def test_log_prob_exists(sir_ode):
+    """
+    Tests if the log_prob exists at the observed site.
+    """
+    S_obs = torch.tensor(10.0)
+    data = {"S_obs": S_obs}
     with pyro.poutine.trace() as tr:
         with SimulatorEventLoop():
-            with PointObservation(time=2.9, loglikelihood=loglikelihood):
-                simulate(sir_ode, init_state, tspan)
+            with PointObservation(time=2.9, data=data):
+                result = simulate(sir_ode, init_state, tspan)
 
-    time_key = tr.trace.nodes.keys().pop()
-    assert isinstance(tr.trace.nodes[time_key]["fn"].log_factor, torch.Tensor)
+    assert isinstance(tr.trace.log_prob_sum(), torch.Tensor), "No log_prob found!"
+
+
+def test_tspan_collision(sir_ode):
+    """
+    Tests if observation times that intersect with tspan do not raise an error or create
+    shape mismatches.
+    """
+    S_obs = torch.tensor(10.0)
+    data = {"S_obs": S_obs}
+    with SimulatorEventLoop():
+        with PointObservation(time=tspan[1], data=data):
+            result = simulate(sir_ode, init_state, tspan)
+
+    assert result.S.shape[0] == 4
+    assert result.I.shape[0] == 4
+    assert result.R.shape[0] == 4
