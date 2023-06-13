604c2d72e72e5d9e082e78ac903850e4c622f980
Eli <eli@elibingham.com>
2023-06-12 21:13:38 -0400

    product

--- causal_pyro/effectful/ops/interpretations.py
+++ causal_pyro/effectful/ops/interpretations.py
@@ -26,6 +26,13 @@ class Operation(Generic[T]):
         return interpret(*args, **kwargs)
 
 
+class _OpInterpretation(Generic[T]):
+    interpret: OpInterpretation[T]
+
+    def __call__(self, result: Optional[T], *args: T) -> T:
+        return self.interpret(result, *args)
+
+
 class Runtime(Generic[T]):
     interpretation: Interpretation[T]
 
@@ -49,42 +56,28 @@ def define_define(Operation, constructors: Interpretation[T]):
         constructors[m] = define(Operation)(fn)
         return define(m)
 
-    global RUNTIME
     RUNTIME = Runtime(constructors)
 
     metadef(define, metadef)
-    return define
+    return RUNTIME, define
 
 
-define = define_define(Operation, Interpretation())
+RUNTIME, define = define_define(Operation, Interpretation())
 
 
-# @define(Operation)  # TODO
+@define(Operation)
 def get_interpretation() -> Interpretation[T]:
-    global RUNTIME
     return RUNTIME.interpretation
 
 
-# @define(Operation)  # TODO
+@define(Operation)
 def swap_interpretation(intp: Interpretation[T]) -> Interpretation[T]:
-    global RUNTIME
     old_intp = RUNTIME.interpretation
     RUNTIME.interpretation = intp
     return old_intp
 
 
-@contextlib.contextmanager
-def handle(intp: Interpretation[T]):
-    new_intp = compose(get_interpretation(), intp)
-    old_intp = swap_interpretation(new_intp)
-    try:
-        yield new_intp
-    finally:
-        swap_interpretation(old_intp)
-
-
 ########################################
-
 
 @define(Operation)
 def compose(intp: Interpretation[T], *intps: Interpretation[T]) -> Interpretation[T]:
@@ -98,7 +91,7 @@ def compose(intp: Interpretation[T], *intps: Interpretation[T]) -> Interpretatio
             [(op, compose_op_interpretation(intp[op], intp2[op])) for op in set(intp.keys()) & set(intp2.keys())]
         )
     else:
-        return functools.reduce(compose, intps)
+        return compose(intp, compose(*intps))
 
 
 @define(Operation)
@@ -108,41 +101,19 @@ def compose_op_interpretation(intp1: OpInterpretation[T], intp2: OpInterpretatio
 
 @define(Operation)
 def fwd(result: Optional[T]) -> T:
-    return result  # reflect(result)  # TODO
+    return result
 
 
-@define(Operation)
-def product(intp: Interpretation[T], *intps: Interpretation[T]) -> Interpretation[T]:
-    if len(intps) == 0:
-        return intp
-    elif len(intps) == 1:
-        intp2, = intps
-        return Interpretation(
-            ((op, product_op_interpretation(intp, intp2[op])) for op in intp2.keys())
-        )
-    else:
-        return functools.reduce(product, intps)
-
-
-@define(Operation)
-def product_op_interpretation(intp1: Interpretation[T], intp2: OpInterpretation[T]) -> OpInterpretation[T]:
-    return ProductOpInterpretation(intp2, intp1)
-
-
-@define(Operation)
-def reflect(result: Optional[T]) -> T:
-    return result if result is not None else fwd(result)
+@contextlib.contextmanager
+def handle(intp: Interpretation[T]):
+    old_intp = swap_interpretation(compose(get_interpretation(), intp))
+    try:
+        yield intp
+    finally:
+        swap_interpretation(old_intp)
 
 
 ##################################################
-
-
-class _OpInterpretation(Generic[T]):
-    interpret: OpInterpretation[T]
-
-    def __call__(self, result: Optional[T], *args: T) -> T:
-        return self.interpret(result, *args)
-
 
 class FwdInterpretation(Generic[T], _OpInterpretation[T]):
     rest: OpInterpretation[T]
@@ -152,12 +123,7 @@ class FwdInterpretation(Generic[T], _OpInterpretation[T]):
         self._active_args = args
 
     def interpret(self, fwd_res: Optional[T], result: Optional[T]) -> T:
-        fwder = Interpretation(((fwd, lambda _, res: res),))  # TODO should be using reflect
-        try:
-            prev = swap_interpretation(compose(get_interpretation(), fwder))
-            return self.rest(result, *self._active_args)
-        finally:
-            swap_interpretation(prev)
+        return self.rest(result, *self._active_args)
 
 
 class ComposeOpInterpretation(Generic[T], _OpInterpretation[T]):
@@ -173,54 +139,78 @@ class ComposeOpInterpretation(Generic[T], _OpInterpretation[T]):
         # 1. creating interpretation for fwd() that calls rest
         # 2. right-composing that with the active interpretation
         # 3. calling the op interpretation
-        fwder = Interpretation(((fwd, FwdInterpretation(self.rest, args)),))
-        try:
-            prev = swap_interpretation(compose(get_interpretation(), fwder))
+        with handle(Interpretation(((fwd, FwdInterpretation(self.rest, args)),))):
             return self.fst(result, *args)
-        finally:
-            swap_interpretation(prev)
 
+
+##################################################
+
+@define(Operation)
+def product(intp: Interpretation[T], *intps: Interpretation[T]) -> Interpretation[T]:
+    if len(intps) == 0:
+        return intp
+    elif len(intps) == 1:
+        intp2, = intps
+        return Interpretation(
+            ((op, product_op_interpretation(intp, intp[op], intp2[op])) for op in intp2.keys())
+        )
+    else:
+        return product(intp, product(*intps))
+
+
+@define(Operation)
+def product_op_interpretation(intp1: Interpretation[T], refl: OpInterpretation[T], intp2: OpInterpretation[T]) -> OpInterpretation[T]:
+    return ProductOpInterpretation(intp1, refl, intp2)
+
+
+@define(Operation)
+def reflect(result: Optional[T]) -> T:
+    return result
+
+
+@contextlib.contextmanager
+def runtime(intp: Interpretation[T]):
+    old_intp = swap_interpretation(product(get_interpretation(), intp))
+    try:
+        yield intp
+    finally:
+        swap_interpretation(old_intp)
+
+
+##################################################
 
 class ReflectInterpretation(Generic[T], _OpInterpretation[T]):
     other: Interpretation[T]
+    rest: OpInterpretation[T]
 
-    def __init__(self, other: Interpretation[T]):
+    def __init__(self, other: Interpretation[T], rest: OpInterpretation[T], args: tuple[T, ...]):
         self.other = other
+        self.rest = rest
+        self._active_args = args
 
     def interpret(self, ref_result: Optional[T], result: Optional[T]) -> T:
-        old_intp = Interpretation((
-            (op, op_intp) for op, op_intp in get_interpretation().items()
-            if op not in self.other
-        ))
-        new_intp = compose(old_intp, self.other)
-        try:
-            prev = swap_interpretation(new_intp)
-            return fwd(result)  # TODO this will not be correct because fwd's interpretation is not updated
-        finally:
-            swap_interpretation(prev)
+        with handle(self.other):
+            return self.rest(result, *self._active_args)
 
 
 class ProductOpInterpretation(Generic[T], _OpInterpretation[T]):
-    fst: OpInterpretation[T]
     other: Interpretation[T]
+    rest: OpInterpretation[T]
+    fst: OpInterpretation[T]
+
+    def __init__(self, other, rest, fst):
+        self.other = other
+        self.rest = rest
+        self.fst = fst
 
     def interpret(self, result: Optional[T], *args: T) -> T:
         # reduces to compose by:
         # 1. creating interpretation that reflect()s each included op
-        # 2. right-composing that with the active interpretation
-        # 3. creating interpretation for reflect() that switches the active interpretation to other
-        # 4. right-composing that with the active interpretation
-        # 5. calling the op interpretation
-        try:
-            reflector = Interpretation(((reflect, ReflectInterpretation(self.other)),))
-            # reflector = compose(
-            #     Interpretation(((op, lambda result, *args: reflect(result)) for op in self.other.keys())),
-            #     reflector
-            # )
-            prev = swap_interpretation(compose(get_interpretation(), reflector))
+        # 2. creating interpretation for reflect() that switches the active interpretation to other
+        # 3. right-composing these with the active interpretation
+        # 4. calling the op interpretation
+        with handle(Interpretation(((reflect, ReflectInterpretation(self.other, self.rest, args)),))):
             return self.fst(result, *args)
-        finally:
-            swap_interpretation(prev)
 
 
 ##################################################
@@ -259,12 +249,24 @@ if __name__ == "__main__":
     default = Interpretation({add: lambda res, *args: res if res is not None else add.body(*args)})
 
     # printme3 = compose(compose(default, printme1), printme2)
-    printme3 = compose(default, compose(printme1, printme2))
+    # printme3 = compose(default, compose(printme1, printme2))
+    printme3 = compose(default, printme1, printme2)
 
     print(add(3, 4))
     print(add3(3, 4, 5))
 
     with handle(printme3) as h:
         print(add(3, 4))
+
+
+    # what should happen with runtime?
+    # when reflect is called, it should jump to the next runtime
+    # i.e. re-invoke the operation under the next runtime interpretation
+
+    with runtime(default):
+        with handle(compose(printme1, printme2)) as h:
+            print(add(3, 4))
+
+    language = product(default, compose(printme1, printme2))
 
     assert False
