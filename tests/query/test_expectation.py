990455425bceadbb369e106c68792df404eea730
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-06-15 00:16:19 +0200

    expectation test passed

--- tests/query/test_expectation.py
+++ tests/query/test_expectation.py
@@ -1,3 +1,5 @@
+import unittest
+import math
 import pyro
 from pyro.distributions import Normal
 from causal_pyro.query.ops import expectation
@@ -5,11 +7,22 @@ from causal_pyro.query.ops import expectation
 from causal_pyro.query.handlers import MonteCarloIntegration
 
 
-def model():
-    x = pyro.sample("x", Normal(0, 1))
-    y = pyro.sample("y", Normal(x, 1))
-    return y
+class TestExpectation(unittest.TestCase):
+
+    def test_MoteCarloIntegration_simple(self):
+
+        def model():
+            x = pyro.sample("x", Normal(1, 1))
+            y = pyro.sample("y", Normal(x-1, 1))
+            return y
+
+        with MonteCarloIntegration(1000):
+            result_x = expectation(model, "x",  0, tuple(), {})
+            result_y = expectation(model, "y",  0, tuple(), {})
+
+        self.assertTrue(math.isclose(result_x, 1, abs_tol=0.1))
+        self.assertTrue(math.isclose(result_y, 0, abs_tol=0.1))
 
 
-with MonteCarloIntegration(100):
-    result = expectation(model, "x", tuple())
+if __name__ == "__main__":
+    unittest.main()
