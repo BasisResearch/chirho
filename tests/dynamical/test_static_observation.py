bcebe3fd08cb985598b9e1a79024711dffdf8e2e
Raj Agrawal <r.agrawal.mit@gmail.com>
2023-06-15 15:36:11 -0400

    test with exit stack works too...

--- tests/dynamical/test_static_observation.py
+++ tests/dynamical/test_static_observation.py
@@ -3,6 +3,7 @@ import logging
 import pyro
 import pytest
 import torch
+from contextlib import ExitStack
 from pyro.infer.autoguide import AutoMultivariateNormal
 from pyro.infer import SVI, Trace_ELBO
 
@@ -73,7 +74,7 @@ def test_tspan_collision(model):
 
 
 @pytest.mark.parametrize("model", [bayes_sir_model])
-def test_svi_composition(model):
+def test_svi_composition_test_one(model):
     data1 = {
         "S_obs": torch.tensor(10.0),
         "I_obs": torch.tensor(5.0),
@@ -102,3 +103,45 @@ def test_svi_composition(model):
     pyro.clear_param_store()
     for step in range(n_steps):
         loss = svi.step()
+
+
+@pytest.mark.parametrize("model", [bayes_sir_model])
+def test_svi_composition_test_two(model):
+    data1 = {
+        "S_obs": torch.tensor(10.0),
+        "I_obs": torch.tensor(5.0),
+        "R_obs": torch.tensor(5.0),
+    }
+    data2 = {
+        "S_obs": torch.tensor(8.0),
+        "I_obs": torch.tensor(6.0),
+        "R_obs": torch.tensor(6.0),
+    }
+
+    data = dict()
+    data[0] = [torch.tensor(2.9), data1]
+    data[1] = [torch.tensor(3.1), data2]
+
+    def conditioned_sir(data):
+        sir = model()
+        observation_managers = []
+        for obs in data.values():
+            obs_time = obs[0].item()
+            obs_data = obs[1]
+            observation_managers.append(PointObservation(obs_time, obs_data))
+        with SimulatorEventLoop():
+            with ExitStack() as stack:
+                for manager in observation_managers:
+                    stack.enter_context(manager)
+                traj = simulate(sir, init_state, tspan)
+        return traj
+
+    guide = AutoMultivariateNormal(conditioned_sir)
+    adam = pyro.optim.Adam({"lr": 0.03})
+    svi = SVI(conditioned_sir, guide, adam, loss=Trace_ELBO())
+    n_steps = 2
+
+    # Do gradient steps
+    pyro.clear_param_store()
+    for step in range(n_steps):
+        loss = svi.step(data)
