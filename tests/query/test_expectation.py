0f97b278a6cb22ad1c9d50facdf29bf2c0bb4c3e
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-06-19 16:01:32 +0200

    linting switched to black

--- tests/query/test_expectation.py
+++ tests/query/test_expectation.py
@@ -31,15 +31,10 @@ class TestExpectation(unittest.TestCase):
         self.assertTrue(math.isclose(result_y, 0, abs_tol=0.15))
 
     def test_MonteCarloIntegration_cancer_with_args(self):
-
         def smoking_model(stress_pt, smokes_cpt, cancer_cpt):
             stress = pyro.sample("stress", Bernoulli(stress_pt)).long()
-            smokes = pyro.sample(
-                "smokes", Bernoulli(smokes_cpt[stress])
-            ).long()
-            cancer = pyro.sample(
-                "cancer", Bernoulli(cancer_cpt[stress, smokes])
-            ).long()
+            smokes = pyro.sample("smokes", Bernoulli(smokes_cpt[stress])).long()
+            cancer = pyro.sample("cancer", Bernoulli(cancer_cpt[stress, smokes])).long()
             return cancer
 
         with MonteCarloIntegration(1000):
@@ -48,22 +43,23 @@ class TestExpectation(unittest.TestCase):
         self.assertTrue(math.isclose(result_cancer, 0.47, abs_tol=0.15))
 
     def test_MonteCarloIntegration_cancer_with_plate(self):
-
         def smoking_model_plate(stress_pt, smokes_cpt, cancer_cpt):
             with pyro.plate("plate", 5, dim=-1):
                 stress = pyro.sample("stress", Bernoulli(stress_pt)).long()
-                smokes = pyro.sample(
-                    "smokes", Bernoulli(smokes_cpt[stress])
-                ).long()
+                smokes = pyro.sample("smokes", Bernoulli(smokes_cpt[stress])).long()
                 cancer = pyro.sample(
                     "cancer", Bernoulli(cancer_cpt[stress, smokes])
                 ).long()
                 return stress, smokes, cancer
 
         with MonteCarloIntegration(1000):
-            result_cancer_plate = expectation(smoking_model_plate, "cancer", 0, probs, {})
+            result_cancer_plate = expectation(
+                smoking_model_plate, "cancer", 0, probs, {}
+            )
 
-        self.assertTrue(all([math.isclose(res, 0.47, abs_tol=0.15) for res in result_cancer_plate]))
+        self.assertTrue(
+            all([math.isclose(res, 0.47, abs_tol=0.15) for res in result_cancer_plate])
+        )
 
 
 if __name__ == "__main__":
