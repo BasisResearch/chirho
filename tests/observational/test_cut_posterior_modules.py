290e324b7681ce097333c58449dc5708ff74f6db
Raj Agrawal <r.agrawal.mit@gmail.com>
2023-06-21 18:28:40 -0400

    passes closed form discrete test case

--- tests/observational/test_cut_posterior_modules.py
+++ tests/observational/test_cut_posterior_modules.py
@@ -1,3 +1,4 @@
+import math
 import pyro
 import pyro.distributions as dist
 import pyro.infer.reparam
@@ -12,15 +13,48 @@ from causal_pyro.observational.handlers import (
     cut,
 )
 
+# Observed data assumed to compute posterior in closed form
+BERN_DATA = {"z": torch.tensor(1.0), "w": torch.tensor(1.0)}
+
 
 @pyro.infer.config_enumerate
-def model():
+def bern_model():
     eta = pyro.sample("eta", dist.Bernoulli(0.5))
     w = pyro.sample("w", dist.Bernoulli(torch.where(eta == 1, 0.8, 0.2)))
     theta = pyro.sample("theta", dist.Bernoulli(0.5))
     z = pyro.sample(
         "z", dist.Bernoulli(torch.where((eta == 1) & (theta == 1), 0.8, 0.2))
     )
+
+
+def bern_posterior():
+    p_eta = dist.Bernoulli(10 / 11)  # pr(eta | w=1, z=1)
+
+    def p_theta_cond_eta(eta):  # pr(theta | eta, z=1)
+        if eta == torch.tensor(1.0):
+            return dist.Bernoulli(0.8)
+        elif eta == torch.tensor(0.0):
+            return dist.Bernoulli(0.5)
+        else:
+            raise ValueError("eta must be 0 or 1")
+
+    return p_eta, p_theta_cond_eta
+
+
+def bern_cut_posterior():
+    # pcut(eta, theta | w=1, z=1) = pr(eta | w=1) x pr(theta | eta, z=1),
+    # where pr() denotes the posterior distribution
+    p_eta = dist.Bernoulli(0.8)  # pr(eta | w=1)
+
+    def p_theta_cond_eta(eta):  # pr(theta | eta, z=1)
+        if eta == torch.tensor(1.0):
+            return dist.Bernoulli(0.8)
+        elif eta == torch.tensor(0.0):
+            return dist.Bernoulli(0.5)
+        else:
+            raise ValueError("eta must be 0 or 1")
+
+    return p_eta, p_theta_cond_eta
 
 
 def linear_gaussian_model(N1=10, N2=10):
@@ -43,81 +77,123 @@ def linear_gaussian_cut_posterior(data):
     return pr_eta_cut, pr_theta_cut_cond_eta
 
 
-def test_simple_two_module_example():
-conditioned_model = pyro.condition(
-    model, data={"z": torch.tensor(1.0), "w": torch.tensor(1.0)}
-)
-module_one_vars = ["eta", "w"]
-module_one, module_two = cut(conditioned_model, vars=module_one_vars)
-
-# Exact posterior inference in module_one
-module_one_post = pyro.infer.infer_discrete(module_one, first_available_dim=-2)
-module_one_samples = []
-for _ in range(1000):
-    with pyro.poutine.trace() as tr1:
-        module_one_post()
-    module_one_samples.append(tr1.trace.nodes["eta"]["value"].item())
-
-# Exact posterior inference in joint model
-joint_post = pyro.infer.infer_discrete(conditioned_model, first_available_dim=-2)
-joint_samples = []
-for _ in range(1000):
-    with pyro.poutine.trace() as tr1:
-        joint_post()
-    joint_samples.append(tr1.trace.nodes["eta"]["value"].item())
-
-torch.tensor(module_one_samples).mean()
-torch.tensor(joint_samples).mean()
-
-# Condition module two on module one
-module_two = pyro.condition(module_two, data=tr1.trace)
-module_two_post = pyro.infer.infer_discrete(module_two, first_available_dim=-2)
-
-with pyro.poutine.trace() as tr2:
-    module_two_post()
-
-
-def module_raises_assertion_error():
-    conditioned_model = pyro.condition(model, data={"z": torch.tensor(1.0), "w": torch.tensor(1.0)})
+def test_cut_module_discrete():
+    conditioned_model = pyro.condition(bern_model, data=BERN_DATA)
     module_one_vars = ["eta", "w"]
     module_one, module_two = cut(conditioned_model, vars=module_one_vars)
-    try:
-        module_two_post = pyro.infer.infer_discrete(module_two, first_available_dim=-2)
-        assert False, "AssertionError should have been raises since module_one is not conditioned on"
-    except AssertionError:
-        print("AssertionError raised as expected")
+    module_one_post = pyro.infer.infer_discrete(module_one, first_available_dim=-2)
+    module_one_samples = []
 
+    # Module 1 check
+    for _ in range(1000):
+        with pyro.poutine.trace() as tr:
+            module_one_post()
+        module_one_samples.append(tr.trace.nodes["eta"]["value"].item())
 
-
-
-
-# TODO: test with VI cut posterior approach in Yu et al (2022) on several simple examples
-
-
-def test_plate_cut_module_discrete_runs():
-    conditioned_model = pyro.condition(
-        model, data={"z": torch.tensor(1.0), "w": torch.tensor(1.0)}
+    rand_error_tol = 3 * 0.5 / math.sqrt(1000)
+    assert (
+        torch.abs(torch.tensor(module_one_samples).mean() - 0.8).item() < rand_error_tol
     )
-    module_one_vars = ["eta", "w"]
-    with IndexPlatesMessenger(), IndexCutModule(module_one_vars):
-        z = conditioned_model()
+
+    # Module 2 check (eta = 1)
+    module_two_cond = pyro.condition(module_two, data={"eta": torch.tensor(1.0)})
+    module_two_post = pyro.infer.infer_discrete(module_two_cond, first_available_dim=-2)
+    module_two_samples = []
+    for _ in range(1000):
+        with pyro.poutine.trace() as tr:
+            module_two_post()
+        module_two_samples.append(tr.trace.nodes["theta"]["value"].item())
+
+    assert (
+        torch.abs(torch.tensor(module_two_samples).mean() - 0.8).item() < rand_error_tol
+    )
+
+    # Module 2 check (eta = 0)
+    module_two_cond = pyro.condition(module_two, data={"eta": torch.tensor(0.0)})
+    module_two_post = pyro.infer.infer_discrete(module_two_cond, first_available_dim=-2)
+    module_two_samples = []
+    for _ in range(1000):
+        with pyro.poutine.trace() as tr:
+            module_two_post()
+        module_two_samples.append(tr.trace.nodes["theta"]["value"].item())
+
+    assert (
+        torch.abs(torch.tensor(module_two_samples).mean() - 0.5).item() < rand_error_tol
+    )
 
 
-def test_plate_cut_module_continuous_runs():
-    data = linear_gaussian_model()
-    conditioned_model = pyro.condition(linear_gaussian_model, data=data)
-    module_one_vars = ["eta", "w"]
-    with IndexPlatesMessenger(), IndexCutModule(module_one_vars):
-        z = conditioned_model()
+# def test_simple_two_module_example():
+# conditioned_model = pyro.condition(
+#     bern_model, data={"z": torch.tensor(1.0), "w": torch.tensor(1.0)}
+# )
+# module_one_vars = ["eta", "w"]
+# module_one, module_two = cut(conditioned_model, vars=module_one_vars)
+
+# # Exact posterior inference in module_one
+# module_one_post = pyro.infer.infer_discrete(module_one, first_available_dim=-2)
+# module_one_samples = []
+# for _ in range(1000):
+#     with pyro.poutine.trace() as tr1:
+#         module_one_post()
+#     module_one_samples.append(tr1.trace.nodes["eta"]["value"].item())
+
+# # Exact posterior inference in joint model
+# joint_post = pyro.infer.infer_discrete(conditioned_model, first_available_dim=-2)
+# joint_samples = []
+# for _ in range(1000):
+#     with pyro.poutine.trace() as tr1:
+#         joint_post()
+#     joint_samples.append(tr1.trace.nodes["eta"]["value"].item())
+
+# torch.tensor(module_one_samples).mean()
+# torch.tensor(joint_samples).mean()
+
+# # Condition module two on module one
+# module_two = pyro.condition(module_two, data=tr1.trace)
+# module_two_post = pyro.infer.infer_discrete(module_two, first_available_dim=-2)
+
+# with pyro.poutine.trace() as tr2:
+#     module_two_post()
 
 
-def test_linear_gaussian_cut():
-    """
-    Tests if the exact cut posterior known in closed form for the linear Gaussian case
-    matches the cut posterior obtained by running inference in the cut module.
-    """
-    pass
-    # N1 = 3
-    # N2 = 3
-    # data = linear_gaussian_model(N1, N2)
-    # exact_cut_posterior = linear_gaussian_cut_posterior(data)
+# def module_raises_assertion_error():
+#     conditioned_model = pyro.condition(model, data={"z": torch.tensor(1.0), "w": torch.tensor(1.0)})
+#     module_one_vars = ["eta", "w"]
+#     module_one, module_two = cut(conditioned_model, vars=module_one_vars)
+#     try:
+#         module_two_post = pyro.infer.infer_discrete(module_two, first_available_dim=-2)
+#         assert False, "AssertionError should have been raises since module_one is not conditioned on"
+#     except AssertionError:
+#         print("AssertionError raised as expected")
+
+
+# # TODO: test with VI cut posterior approach in Yu et al (2022) on several simple examples
+
+
+# def test_plate_cut_module_discrete_runs():
+#     conditioned_model = pyro.condition(
+#         bern_model, data={"z": torch.tensor(1.0), "w": torch.tensor(1.0)}
+#     )
+#     module_one_vars = ["eta", "w"]
+#     with IndexPlatesMessenger(), IndexCutModule(module_one_vars):
+#         z = conditioned_model()
+
+
+# def test_plate_cut_module_continuous_runs():
+#     data = linear_gaussian_model()
+#     conditioned_model = pyro.condition(linear_gaussian_model, data=data)
+#     module_one_vars = ["eta", "w"]
+#     with IndexPlatesMessenger(), IndexCutModule(module_one_vars):
+#         z = conditioned_model()
+
+
+# def test_linear_gaussian_cut():
+#     """
+#     Tests if the exact cut posterior known in closed form for the linear Gaussian case
+#     matches the cut posterior obtained by running inference in the cut module.
+#     """
+#     pass
+#     # N1 = 3
+#     # N2 = 3
+#     # data = linear_gaussian_model(N1, N2)
+#     # exact_cut_posterior = linear_gaussian_cut_posterior(data)
