5ff2d47e89f4b8026157af92e82af59aac827eb4
Raj Agrawal <r.agrawal.mit@gmail.com>
2023-06-21 23:36:51 -0400

    cleaned up test file

--- tests/observational/test_cut_posterior_modules.py
+++ tests/observational/test_cut_posterior_modules.py
@@ -77,6 +77,36 @@ def linear_gaussian_cut_posterior(data):
     return pr_eta_cut, pr_theta_cut_cond_eta
 
 
+def module_raises_assertion_error():
+    conditioned_model = pyro.condition(
+        model, data={"z": torch.tensor(1.0), "w": torch.tensor(1.0)}
+    )
+    module_one_vars = ["eta", "w"]
+    module_one, module_two = cut(conditioned_model, vars=module_one_vars)
+    try:
+        module_two_post = pyro.infer.infer_discrete(module_two, first_available_dim=-2)
+        assert (
+            False
+        ), "AssertionError should have been raises since module_one is not conditioned on"
+    except AssertionError:
+        print("AssertionError raised as expected")
+
+
+def test_plate_cut_module_runs():
+    # Runs for discrete model
+    conditioned_model = pyro.condition(bern_model, data=BERN_DATA)
+    module_one_vars = ["eta", "w"]
+    with IndexPlatesMessenger(), IndexCutModule(module_one_vars):
+        z = conditioned_model()
+
+    # Runs for continous model
+    data = linear_gaussian_model()
+    conditioned_model = pyro.condition(linear_gaussian_model, data=data)
+    module_one_vars = ["eta", "w"]
+    with IndexPlatesMessenger(), IndexCutModule(module_one_vars):
+        z = conditioned_model()
+
+
 def test_cut_module_discrete():
     conditioned_model = pyro.condition(bern_model, data=BERN_DATA)
     module_one_vars = ["eta", "w"]
@@ -122,78 +152,5 @@ def test_cut_module_discrete():
     )
 
 
-# def test_simple_two_module_example():
-# conditioned_model = pyro.condition(
-#     bern_model, data={"z": torch.tensor(1.0), "w": torch.tensor(1.0)}
-# )
-# module_one_vars = ["eta", "w"]
-# module_one, module_two = cut(conditioned_model, vars=module_one_vars)
-
-# # Exact posterior inference in module_one
-# module_one_post = pyro.infer.infer_discrete(module_one, first_available_dim=-2)
-# module_one_samples = []
-# for _ in range(1000):
-#     with pyro.poutine.trace() as tr1:
-#         module_one_post()
-#     module_one_samples.append(tr1.trace.nodes["eta"]["value"].item())
-
-# # Exact posterior inference in joint model
-# joint_post = pyro.infer.infer_discrete(conditioned_model, first_available_dim=-2)
-# joint_samples = []
-# for _ in range(1000):
-#     with pyro.poutine.trace() as tr1:
-#         joint_post()
-#     joint_samples.append(tr1.trace.nodes["eta"]["value"].item())
-
-# torch.tensor(module_one_samples).mean()
-# torch.tensor(joint_samples).mean()
-
-# # Condition module two on module one
-# module_two = pyro.condition(module_two, data=tr1.trace)
-# module_two_post = pyro.infer.infer_discrete(module_two, first_available_dim=-2)
-
-# with pyro.poutine.trace() as tr2:
-#     module_two_post()
-
-
-# def module_raises_assertion_error():
-#     conditioned_model = pyro.condition(model, data={"z": torch.tensor(1.0), "w": torch.tensor(1.0)})
-#     module_one_vars = ["eta", "w"]
-#     module_one, module_two = cut(conditioned_model, vars=module_one_vars)
-#     try:
-#         module_two_post = pyro.infer.infer_discrete(module_two, first_available_dim=-2)
-#         assert False, "AssertionError should have been raises since module_one is not conditioned on"
-#     except AssertionError:
-#         print("AssertionError raised as expected")
-
-
-# # TODO: test with VI cut posterior approach in Yu et al (2022) on several simple examples
-
-
-# def test_plate_cut_module_discrete_runs():
-#     conditioned_model = pyro.condition(
-#         bern_model, data={"z": torch.tensor(1.0), "w": torch.tensor(1.0)}
-#     )
-#     module_one_vars = ["eta", "w"]
-#     with IndexPlatesMessenger(), IndexCutModule(module_one_vars):
-#         z = conditioned_model()
-
-
-# def test_plate_cut_module_continuous_runs():
-#     data = linear_gaussian_model()
-#     conditioned_model = pyro.condition(linear_gaussian_model, data=data)
-#     module_one_vars = ["eta", "w"]
-#     with IndexPlatesMessenger(), IndexCutModule(module_one_vars):
-#         z = conditioned_model()
-
-
-# def test_linear_gaussian_cut():
-#     """
-#     Tests if the exact cut posterior known in closed form for the linear Gaussian case
-#     matches the cut posterior obtained by running inference in the cut module.
-#     """
-#     pass
-#     # N1 = 3
-#     # N2 = 3
-#     # data = linear_gaussian_model(N1, N2)
-#     # exact_cut_posterior = linear_gaussian_cut_posterior(data)
+def test_index_module_discrete():
+    pass
