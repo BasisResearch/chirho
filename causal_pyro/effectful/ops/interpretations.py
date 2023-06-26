0b8de03191314402bcb462a9ace81b5ebe69f62b
Eli <eli@elibingham.com>
2023-06-26 17:51:57 -0400

    rearrange

--- causal_pyro/effectful/ops/interpretations.py
+++ causal_pyro/effectful/ops/interpretations.py
@@ -13,23 +13,23 @@ T = TypeVar("T")
 
 @define(Operation)
 def fwd(result: Optional[T]) -> T:
-    return reflect(result)
+    return reflect(result)  # TODO should fwd default to reflect like this?
 
 
 @define(Operation)
 def compose(intp: Interpretation[T], *intps: Interpretation[T]) -> Interpretation[T]:
     if len(intps) == 0:
-        return intp
-    elif len(intps) == 1:
-        intp2, = intps
-        return define(Interpretation)(
-            [(op, intp[op]) for op in set(intp.keys()) - set(intp2.keys())] +
-            [(op, intp2[op]) for op in set(intp2.keys()) - set(intp.keys())] +
-            [(op, functools.partial(reset_prompt, fwd, intp[op], intp2[op]))
-             for op in set(intp.keys()) & set(intp2.keys())]
-        )
-    else:
-        return compose(intp, compose(*intps))
+        return intp  # unit
+    elif len(intps) > 1:
+        return compose(intp, compose(*intps))  # associativity
+
+    intp2, = intps
+    return define(Interpretation)(
+        [(op, intp[op]) for op in set(intp.keys()) - set(intp2.keys())] +
+        [(op, intp2[op]) for op in set(intp2.keys()) - set(intp.keys())] +
+        [(op, functools.partial(reset_prompt, fwd, intp[op], intp2[op]))
+            for op in set(intp.keys()) & set(intp2.keys())]
+    )
 
 
 ##################################################
@@ -42,37 +42,36 @@ def reflect(result: Optional[T]) -> T:
 @define(Operation)
 def product(intp: Interpretation[T], *intps: Interpretation[T]) -> Interpretation[T]:
     if len(intps) == 0:
-        return intp
-    elif len(intps) == 1:
-        intp2, = intps
+        return intp  # unit
+    elif len(intps) > 1:
+        return product(intp, product(*intps))  # associativity
 
-        # compose interpretations with reflections to ensure compatibility with compose()
-        # TODO use interpreter instead of compose here to disentangle compose and product
-        refls1 = compose(
-            prompt_calls(reflect, *intp.keys()),
-            define(Interpretation)({op: intp2[op] for op in intp2.keys() if op in intp})
+    # cases:
+    # 1. op in intp2 but not intp: handle from scratch when encountered in latent context
+    # 2. op in intp but not intp2: don't expose in user code
+    # 3. op in both intp and intp2: use intp[op] under intp and intp2[op] under intp2 as continuations
+    intp2, = intps
+
+    # compose interpretations with reflections to ensure compatibility with compose()
+    # TODO use interpreter instead of compose here to disentangle compose and product
+    refls1 = compose(
+        prompt_calls(reflect, *intp.keys()),
+        define(Interpretation)({op: intp2[op] for op in intp2.keys() if op in intp})
+    )
+    refls2 = compose(prompt_calls(reflect, *intp.keys(), *intp2.keys()), intp)
+
+    # on reflect, jump to the outer interpretation and interpret it using itself
+    return define(Interpretation)({
+        op: functools.partial(
+            reset_prompt,
+            reflect,
+            # TODO is this call to interpret correct for nested products?
+            interpreter(refls2)(lambda v, *args, **kwargs: op(*args, **kwargs) if v is None else v),
+            # TODO is this call to interpret correct for nested products? is it even necessary?
+            interpreter(refls1)(intp2[op])
         )
-        refls2 = compose(prompt_calls(reflect, *intp.keys(), *intp2.keys()), intp)
-
-        # cases:
-        # 1. op in intp2 but not intp: handle from scratch when encountered in latent context
-        # 2. op in intp but not intp2: don't expose in user code
-        # 3. op in both intp and intp2: use intp[op] under intp and intp2[op] under intp2 as continuations
-
-        # on reflect, jump to the outer interpretation and interpret it using itself
-        return define(Interpretation)({
-            op: functools.partial(
-                reset_prompt,
-                reflect,
-                # TODO is this call to interpret correct for nested products?
-                interpreter(refls2)(lambda v, *args, **kwargs: op(*args, **kwargs) if v is None else v),
-                # TODO is this call to interpret correct for nested products? is it even necessary?
-                interpreter(refls1)(intp2[op])
-            )
-            for op in intp2.keys()
-        })
-    else:
-        return product(intp, product(*intps))
+        for op in intp2.keys()
+    })
 
 
 ############################################################
