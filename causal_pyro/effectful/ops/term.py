a26cfa0dae3d3f3b9370a40504eb8c11f01bccec
Eli <eli@elibingham.com>
2023-06-27 11:26:56 -0400

    kwargs

--- causal_pyro/effectful/ops/term.py
+++ causal_pyro/effectful/ops/term.py
@@ -1,4 +1,6 @@
-from typing import Generic, Iterable, Protocol, TypeVar, runtime_checkable
+from typing import Generic, Iterable, Mapping, Protocol, TypeVar
+
+import typing
 
 from causal_pyro.effectful.ops.environment import Variable
 from causal_pyro.effectful.ops.interpretation import Interpretation, register
@@ -9,23 +11,33 @@ S = TypeVar("S")
 T = TypeVar("T")
 
 
-@runtime_checkable
+@typing.runtime_checkable
 class Term(Protocol[T]):
     __head__: Operation[T]
-    __args__: tuple["Term[T]" | Variable[T] | T, ...]
+    __args__: Iterable["Term[T]" | Variable[T] | T]
+    __kwargs__: Mapping[str, "Term[T]" | Variable[T] | T]
 
 
 @register(define(Term))
 class _BaseTerm(Generic[T], Term[T]):
     __head__: Operation[T]
     __args__: tuple["Term[T]" | Variable[T] | T, ...]
+    __kwargs__: dict[str, "Term[T]" | Variable[T] | T]
 
-    def __init__(self, head: Operation[T], args: Iterable[Term[T] | Variable[T] | T]):
+    def __init__(
+        self,
+        head: Operation[T],
+        args: Iterable[Term[T] | Variable[T] | T],
+        kwargs: Mapping[str, Term[T] | Variable[T] | T]
+    ):
         self.__head__ = head
         self.__args__ = tuple(args)
+        self.__kwargs__ = dict(kwargs)
 
     def __repr__(self) -> str:
-        return f"{self.__head__}({', '.join(map(repr, self.__args__))})"
+        return f"{self.__head__}(" + \
+            f"{', '.join(map(repr, self.__args__))}," + \
+            f"{', '.join(f'{k}={v}' for k, v in self.__kwargs__.items())})"
 
 
 @define(Operation)
@@ -39,8 +51,13 @@ def args_of(term: Term[T]) -> Iterable[Term[T] | Variable[T] | T]:
 
 
 @define(Operation)
+def kwargs_of(term: Term[T]) -> Mapping[str, Term[T] | Variable[T] | T]:
+    return term.__kwargs__
+
+
+@define(Operation)
 def LazyInterpretation(*ops: Operation[T]) -> Interpretation[T | Term[T] | Variable[T]]:
     return define(Interpretation)({
-        op: lambda _, *args: define(Term)(op, args)
+        op: lambda _, *args, **kwargs: define(Term)(op, args, kwargs)
         for op in ops
     })
