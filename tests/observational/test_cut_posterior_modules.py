7ecc91832531af54ad827a2640f4916623ea84ad
Raj Agrawal <r.agrawal.mit@gmail.com>
2023-07-24 15:15:27 -0400

    removed discrete test that checks compares svi output

--- tests/observational/test_cut_posterior_modules.py
+++ tests/observational/test_cut_posterior_modules.py
@@ -10,7 +10,6 @@ import pyro.poutine as poutine
 from chirho.indexed.handlers import IndexPlatesMessenger
 from chirho.observational.handlers.soft_conditioning import IndexCutModule, cut
 
-
 # Observed data assumed in the closed-form posterior expressions
 BERN_DATA = {"z": torch.tensor(1.0), "w": torch.tensor(1.0)}
 
@@ -55,27 +54,17 @@ def bern_cut_posterior():
     return p_eta, p_theta_cond_eta
 
 
-def linear_gaussian_model(N1=10, N2=10):
+def linear_gaussian_model(N1=10, N2=10, sigma1=1.0, sigma2=1.0):
     eta = pyro.sample("eta", dist.Normal(0, 1))
     theta = pyro.sample("theta", dist.Normal(0, 1))
     with pyro.plate("module_one_plate", N1):
-        w = pyro.sample("w", dist.Normal(eta, 1))
+        w = pyro.sample("w", dist.Normal(eta, sigma1))
     with pyro.plate("module_two_plate", N2):
-        z = pyro.sample("z", dist.Normal(eta + theta, 1))
+        z = pyro.sample("z", dist.Normal(eta + theta, sigma2))
     return {"w": w, "z": z}
 
 
-def linear_gaussian_cut_posterior(data):
-    w = data["w"]
-    z = data["z"]
-    pr_eta_cut = dist.Normal(data["w"].sum() / (N1 + 1), scale=1 / torch.sqrt(N1 + 1))
-    pr_theta_cut_cond_eta = lambda eta: dist.Normal(
-        (data["z"] - eta).sum() / (N2 + 1), scale=1 / torch.sqrt(N2 + 1)
-    )
-    return pr_eta_cut, pr_theta_cut_cond_eta
-
-
-def module_raises_assertion_error():
+def test_cut_module_raises_assertion_error():
     conditioned_model = pyro.condition(
         model, data={"z": torch.tensor(1.0), "w": torch.tensor(1.0)}
     )
@@ -85,7 +74,7 @@ def module_raises_assertion_error():
         module_two_post = pyro.infer.infer_discrete(module_two, first_available_dim=-2)
         assert (
             False
-        ), "AssertionError should have been raises since module_one is not conditioned on"
+        ), "AssertionError should have been raised since module_one is not conditioned on"
     except AssertionError:
         print("AssertionError raised as expected")
 
@@ -150,143 +139,6 @@ def test_cut_module_discrete():
     )
 
 
-def compute_exact_elbo_discrete(model, psi):
-    psi_one, psi_two, psi_three = psi
-
-    # Compute probability of each configuration
-    q_00 = (1 - psi_one) * (1 - psi_two)
-    q_01 = (1 - psi_one) * psi_two
-    q_10 = psi_one * (1 - psi_three)
-    q_11 = psi_one * psi_three
-    elbo = torch.tensor(0.0)
-
-    # Add entropy term -E[log q(eta, theta)]
-    elbo -= (
-        q_00 * torch.log(q_00)
-        + q_01 * torch.log(q_01)
-        + q_10 * torch.log(q_10)
-        + q_11 * torch.log(q_11)
-    )
-
-    class FakeTrace(NamedTuple):
-        nodes: dict
-
-        def __contains__(self, name):
-            return name in self.nodes
-
-    def trace_from_values(**values):
-        return FakeTrace(
-            nodes={
-                name: {
-                    "name": name,
-                    "type": "sample",
-                    "value": value,
-                    "is_observed": False,
-                    "infer": {},
-                }
-                for name, value in values.items()
-            }
-        )
-
-    # Add E[log p(eta, theta, w=1, z=1)] term
-    tr00 = {"eta": torch.tensor(0.0), "theta": torch.tensor(0.0)}
-    tr01 = {"eta": torch.tensor(0.0), "theta": torch.tensor(1.0)}
-    tr10 = {"eta": torch.tensor(1.0), "theta": torch.tensor(0.0)}
-    tr11 = {"eta": torch.tensor(1.0), "theta": torch.tensor(1.0)}
-
-    log_p_00 = (
-        poutine.trace(poutine.replay(model, trace=trace_from_values(**tr00)))
-        .get_trace()
-        .log_prob_sum()
-    )
-    log_p_01 = (
-        poutine.trace(poutine.replay(model, trace=trace_from_values(**tr01)))
-        .get_trace()
-        .log_prob_sum()
-    )
-    log_p_10 = (
-        poutine.trace(poutine.replay(model, trace=trace_from_values(**tr10)))
-        .get_trace()
-        .log_prob_sum()
-    )
-    log_p_11 = (
-        poutine.trace(poutine.replay(model, trace=trace_from_values(**tr11)))
-        .get_trace()
-        .log_prob_sum()
-    )
-    elbo += q_00 * log_p_00 + q_01 * log_p_01 + q_10 * log_p_10 + q_11 * log_p_11
-
-    return elbo
-
-
-# def compute_exact_elbo_discrete(model, psi):
-#     psi_one, psi_two, psi_three = psi
-
-#     # Compute probability of each configuration
-#     q_00 = (1 - psi_one) * (1 - psi_two)
-#     q_01 = (1 - psi_one) * psi_two
-#     q_10 = psi_one * (1 - psi_three)
-#     q_11 = psi_one * psi_three
-#     elbo = torch.tensor(0.0)
-
-#     # Add entropy term -E[log q(eta, theta)]
-#     elbo -= (
-#         q_00 * torch.log(q_00)
-#         + q_01 * torch.log(q_01)
-#         + q_10 * torch.log(q_10)
-#         + q_11 * torch.log(q_11)
-#     )
-
-#     # Add E[log p(eta, theta, w=1, z=1)] term
-#     tr00 = {"eta": torch.tensor(0.0), "theta": torch.tensor(0.0)}
-#     tr01 = {"eta": torch.tensor(0.0), "theta": torch.tensor(1.0)}
-#     tr10 = {"eta": torch.tensor(1.0), "theta": torch.tensor(0.0)}
-#     tr11 = {"eta": torch.tensor(1.0), "theta": torch.tensor(1.0)}
-#     log_p_00 = (
-#         poutine.trace(poutine.condition(model, data=tr00)).get_trace().log_prob_sum()
-#     )
-#     log_p_01 = (
-#         poutine.trace(poutine.condition(model, data=tr01)).get_trace().log_prob_sum()
-#     )
-#     log_p_10 = (
-#         poutine.trace(poutine.condition(model, data=tr10)).get_trace().log_prob_sum()
-#     )
-#     log_p_11 = (
-#         poutine.trace(poutine.condition(model, data=tr11)).get_trace().log_prob_sum()
-#     )
-#     elbo += q_00 * log_p_00 + q_01 * log_p_01 + q_10 * log_p_10 + q_11 * log_p_11
-
-#     return elbo
-
-
-def test_index_module_discrete():
-    # psi_one = p(eta = 1)
-    # psi_two = p(theta = 1 | eta = 0)
-    # psi_three = p(theta = 1 | eta = 1)
-    module_one_vars = ["eta", "w"]
-    conditioned_model = pyro.condition(bern_model, data=BERN_DATA)
-    with IndexPlatesMessenger():
-        with IndexCutModule(module_one_vars):
-            psi_one_grid = torch.arange(0, 1, 0.05)
-            psi_two_grid = torch.arange(0, 1, 0.05)
-            psi_three_grid = torch.arange(0, 1, 0.05)
-            best_config = None
-            best_elbo = -float("inf")
-            for psi_one in psi_one_grid:
-                for psi_two in psi_two_grid:
-                    for psi_three in psi_three_grid:
-                        # Compute ELBO loss exactly
-                        elbo = compute_exact_elbo_discrete(
-                            conditioned_model, (psi_one, psi_two, psi_three)
-                        )
-                        if elbo > best_elbo:
-                            best_elbo = elbo
-                            best_config = (psi_one, psi_two, psi_three)
-
-    print(best_config[0])
-    print(best_config[1])
-    print(best_config[2])
-
-    assert torch.abs(best_config[0] - 0.8).item() < 0.025, print(best_config[0])
-    assert torch.abs(best_config[1] - 0.5).item() < 0.025, print(best_config[1])
-    assert torch.abs(best_config[2] - 0.8).item() < 0.025, print(best_config[2])
+def test_zero_log_probs():
+    # Should ensure in cut complement, the gradients are zero for cut module
+    pass
