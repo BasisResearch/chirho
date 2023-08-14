f5ae2fb763e1130777a864e24dc1f57890cbbd5d
Andy Zane <andy@basis.ai>
2023-08-14 18:53:05 -0400

    adds simple tabi notebook for toy example.

---
+++ tests/contrib/expectation_programming/test_magics.py
@@ -0,0 +1,68 @@
+from typing import Callable
+import chirho.contrib.compexp as ep
+import pyro
+import torch
+import pyro.distributions as dist
+from collections import OrderedDict
+
+TT = torch.Tensor
+tt = torch.tensor
+
+d = torch.nn.Parameter(tt([2.]))
+Xv = tt(-1.)
+Yv = tt(2.)
+XYv = OrderedDict(x=Xv, y=Yv)
+
+
+def _cost(stochastics):
+    x = stochastics["x"]
+    y = stochastics["y"]
+    return d[0] * x * y
+
+
+def dirac_xy():
+    x = pyro.sample('x', dist.Delta(Xv))
+    y = pyro.sample('y', dist.Delta(Yv))
+
+    return OrderedDict(x=x, y=y)
+
+
+def test_identity():
+    expected_cost = ep.E(f=_cost, name="c")
+
+    with ep.MonteCarloExpectationHandler(num_samples=1):
+        cost_estimate = expected_cost(dirac_xy)
+        assert torch.isclose(cost_estimate, tt(-4.))
+
+    expected_cost_grad = expected_cost.grad(params=d)
+
+    with ep.MonteCarloExpectationHandler(num_samples=1):
+        cost_grad_estimate = expected_cost_grad(dirac_xy)
+        assert torch.isclose(cost_grad_estimate, tt(-2.))
+
+
+def test_relu():
+    expected_cost_relu_p = ep.E(f=_cost, name="c").relu()
+    expected_cost_relu_n = ep.E(f=lambda s: -_cost(s), name="c").relu()
+
+    with ep.MonteCarloExpectationHandler(num_samples=1):
+        cost_relu_estimate_p = expected_cost_relu_p(dirac_xy)
+        assert torch.isclose(cost_relu_estimate_p, tt(0.))
+        cost_relu_estimate_n = expected_cost_relu_n(dirac_xy)
+        assert torch.isclose(cost_relu_estimate_n, tt(4.))
+
+    expected_cost_relu_p_grad = expected_cost_relu_p.grad(params=d)
+    expected_cost_relu_n_grad = expected_cost_relu_n.grad(params=d)
+
+    with ep.MonteCarloExpectationHandler(num_samples=1):
+        cost_relu_grad_estimate_p = expected_cost_relu_p_grad(dirac_xy)
+        assert torch.isclose(cost_relu_grad_estimate_p, tt(0.))
+        cost_relu_grad_estimate_n = expected_cost_relu_n_grad(dirac_xy)
+        assert torch.isclose(cost_relu_grad_estimate_n, tt(2.))
+
+
+def test_neg_relu_comp():
+    expected_cost_neg_relu = (-ep.E(f=_cost, name="c")).relu()
+
+    with ep.MonteCarloExpectationHandler(num_samples=1):
+        assert torch.isclose(expected_cost_neg_relu(dirac_xy), tt(4.))
