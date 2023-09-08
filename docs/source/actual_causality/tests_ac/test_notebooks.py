141d95816c48862777cd79d63967ed939b01c28a
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-09-08 10:21:00 +0200

    notebook tests, updated notebooks after rebase

---
+++ docs/source/actual_causality/tests_ac/test_notebooks.py
@@ -0,0 +1,12 @@
+import subprocess
+
+notebook_test_commands = [
+    'pytest --nbmake -n=auto "./docs/source/actual_causality/preemption_no_log_prob"',
+    'pytest --nbmake -n=auto "./docs/source/actual_causality/pre_release_versions"',
+]
+
+for command in notebook_test_commands:
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Notebook test command failed: {e}")
