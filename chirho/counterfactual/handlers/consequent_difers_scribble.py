ebb3204534f5aedaa7874ed7465874d23ba980df
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-09-18 14:20:56 +0200

    consequent_differs WIP

--- chirho/counterfactual/handlers/consequent_difers_scribble.py
+++ chirho/counterfactual/handlers/consequent_difers_scribble.py
@@ -5,10 +5,13 @@ from typing import Callable, Iterable, TypeVar
 
 import torch
 import pyro
-from pyro.distributions import Bernoulli
+import pyro.distributions as dist
+import pytest
 
+from chirho.counterfactual.ops import split, preempt
 from chirho.indexed.ops import IndexSet, cond, gather
 from chirho.counterfactual.handlers.selection import get_factual_indices
+from chirho.counterfactual.handlers import MultiWorldCounterfactual
 
 T = TypeVar("T")
 
@@ -25,35 +28,68 @@ def consequent_differs(antecedents: Iterable[str] = [], eps: float = -1e8, event
         )
         not_eq = consequent != gather(consequent, indices, event_dim=event_dim)
         for _ in range(event_dim):
-            not_eq = torch.all(
-                not_eq, dim=-1, keepdim=False)
+            not_eq = torch.all(not_eq, dim=-1, keepdim=False)
         return cond(eps, 0.0, not_eq, event_dim=event_dim)
 
     return _consequent_differs
 
 
-# testing setup
-# Simple model based on the bottle shattering example______
-sally_hits_pt = torch.tensor([1.0])
-bill_hits_cpt = torch.tensor([1.0, 0.0])
-bottle_shatters_cpt = torch.tensor([[0.0, 1.0], [1.0, 1.0]])
-
-probs = (sally_hits_pt, bill_hits_cpt, bottle_shatters_cpt)
 
 
-def bottle_bn(sally_hits_pt, bill_hits_cpt, bottle_shatters_cpt):
-    sh = pyro.sample("sh", Bernoulli(sally_hits_pt)).long()
-    bh = pyro.sample("bh", Bernoulli(bill_hits_cpt[sh])).long()
-    bs = pyro.sample("bs", Bernoulli(bottle_shatters_cpt[sh, bh])).long()
+#@pytest.mark.parametrize("plate_size", [4, 50, 200])
+#@pytest.mark.parametrize("event_shape", [(), (3,), (3, 2)])
+#def test_consequent_differs():
 
-    # bh = bh.float()
-    # bh = bh.float()
-    # bs = bs.float()
-
-    return {"sh": sh, "bh": bh, "bs": bs}
+plate_size = 4
+event_shape = ()
+joint_dims = torch.Size([plate_size, *event_shape])
+case = torch.randint(0, 2, size=joint_dims)
 
 
-def bottle_bn_complete():
-    return bottle_bn(*probs)
+@pyro.plate("data", size=plate_size, dim=-1)
+def model_cd():
+    w = pyro.sample("w", dist.Normal(0, .1).expand(event_shape).to_event(len(event_shape)))
+    print("w", w)
+    new_w = w.clone()
+    new_w[1::2] = 10 
+    print("new_w_", new_w)
+    w = split(w, (new_w,), name="split")
+    print("w after split", w)
+    consequent = pyro.deterministic("consequent", w * .1)
+    print("consequent", consequent)
+    
+    antecedents = ["split"]
+    
+    indices = IndexSet(
+            **{
+                name: ind
+                for name, ind in get_factual_indices().items()
+                if name in antecedents
+            }
+        )
+    print(indices)
+     
+    factual_consequent = gather(consequent, indices, event_dim=0)
+    print("factual_con", factual_consequent)
+    
+    
+    
+    
+    #con_dif = consequent_differs(antecedents=["w"])(consequent)
+    #print("applied to con", con_dif)
 
 
+with MultiWorldCounterfactual() as mwc:
+    with pyro.poutine.trace() as tr:
+        model_cd()
+
+
+# with MultiWorldCounterfactual():
+        
+        
+        
+
+
+#with MultiWorldCounterfactual() as mwc:
+#    with pyro.poutine.trace() as tr:
+        
