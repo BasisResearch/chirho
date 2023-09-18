53a6202f752c2736d5c545df681c02ebc9b2e38a
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-09-18 10:24:57 +0200

    cleanup

--- chirho/counterfactual/handlers/explanation.py
+++ chirho/counterfactual/handlers/explanation.py
@@ -1,34 +1,28 @@
 from typing import Callable, Iterable, TypeVar
-from chirho.indexed.ops import IndexSet, gather, indices_of, scatter
 
+from chirho.indexed.ops import IndexSet, gather, indices_of, scatter
 
 S = TypeVar("S")
 T = TypeVar("T")
 
 
-def undo_split(
-    antecedents: Iterable[str] = None, event_dim: int = 0
-) -> Callable[[T], T]:
+def undo_split(antecedents: Iterable[str] = None, event_dim: int = 0) -> Callable[[T], T]:
     """
-    A helper function that undoes an upstream `chirho.counterfactual.ops.split`
-    operation by gathering the factual value and scattering it back into
-    two alternative cases.
+    A helper function that undoes an upstream `chirho.counterfactual.ops.split` operation,
+    meant to meant to be used to create arguments to pass to `intervene`/`split`/`preempt`.
+    Works by gathering the factual value and scattering it back into two alternative cases.
 
-    :param antecedents: A list of upstream intervened sites which induced
-                        the `split` to be reversed.
-    :param event_dim: The event dimension.
+    :param antecedents: A list of upstream intervened sites which induced the `split` to be reversed.
+    :param event_dim: The event dimension of the value to be preempted.
 
-    :return: A callable that applied to a site value object returns
-            a site value object in which the factual value has been
-            scattered back into two alternative cases.
+    :return: A callable that applied to a site value object returns a site value object in which
+    the factual value has been scattered back into two alternative cases.
     """
     if antecedents is None:
         antecedents = []
 
     def _undo_split(value: T) -> T:
-        antecedents_ = [
-            a for a in antecedents if a in indices_of(value, event_dim=event_dim)
-        ]
+        antecedents_ = [a for a in antecedents if a in indices_of(value, event_dim=event_dim)]
 
         factual_value = gather(
             value,
@@ -38,12 +32,8 @@ def undo_split(
 
         return scatter(
             {
-                IndexSet(
-                    **{antecedent: {0} for antecedent in antecedents_}
-                ): factual_value,
-                IndexSet(
-                    **{antecedent: {1} for antecedent in antecedents_}
-                ): factual_value,
+                IndexSet(**{antecedent: {0} for antecedent in antecedents_}): factual_value,
+                IndexSet(**{antecedent: {1} for antecedent in antecedents_}): factual_value,
             },
             event_dim=event_dim,
         )
