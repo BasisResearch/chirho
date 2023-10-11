131d0c0c4def6fbebf6530bd19db8c0300ed14fd
Eli <eli@elibingham.com>
2023-10-11 16:21:00 -0400

    rename indexed to _patterns

--- chirho/dynamical/internals/__init__.py
+++ chirho/dynamical/internals/__init__.py
@@ -1,5 +1,5 @@
 # Include only imports that are needed for registering dispatches.
 
 from . import dynamical  # noqa: F401
-from . import indexed  # noqa: F401
+from . import _patterns  # noqa: F401
 from . import solver  # noqa: F401
