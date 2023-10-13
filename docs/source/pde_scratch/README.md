17bad99f40c24280767edebf26cfcf80e861e6ba
Andy Zane <andy@basis.ai>
2023-10-13 14:28:25 -0600

    adds demos that need to be combined.

---
+++ docs/source/pde_scratch/README.md
@@ -0,0 +1,2 @@
+This directory is a WIP with scratch code to test how and if we can integrate FEniCS as a PDE solver underpinning
+chirho support for PDEs. It will need to be re-organized into demonstrative notebooks as integration etc. matures.
---
+++ docs/source/pde_scratch/outside_demos/poisson.py
@@ -0,0 +1,77 @@
+# This script copied from:
+# https://github.com/barkm/torch-fenics/blob/master/examples/poisson.py
+
+# HACK when running osx-64 conda env on M2 mac. Side effects could be an issue:
+# https://stackoverflow.com/a/53014308
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+import torch
+
+import torch_fenics
+
+# Import fenics and override necessary data structures with fenics_adjoint
+from fenics import *
+from fenics_adjoint import *
+
+# Declare the FEniCS model corresponding to solving the Poisson equation
+# with variable source term and boundary value
+class Poisson(torch_fenics.FEniCSModule):
+    # Construct variables which can be reused in the constructor
+    def __init__(self):
+        # Call super constructor
+        super().__init__()
+
+        # Create function space
+        mesh = UnitIntervalMesh(20)
+        self.V = FunctionSpace(mesh, 'P', 1)
+
+        # Create trial and test functions
+        u = TrialFunction(self.V)
+        self.v = TestFunction(self.V)
+
+        # Construct bilinear form
+        self.a = inner(grad(u), grad(self.v)) * dx
+
+    def solve(self, f, g):
+        # Construct linear form
+        L = f * self.v * dx
+
+        # Construct boundary condition
+        bc = DirichletBC(self.V, g, 'on_boundary')
+
+        # Solve the Poisson equation
+        u = Function(self.V)
+        solve(self.a == L, u, bc)
+
+        # Return the solution
+        return u
+
+    def input_templates(self):
+        # Declare templates for the inputs to Poisson.solve
+        return Constant(0), Constant(0)
+
+
+if __name__ == '__main__':
+    # Construct the FEniCS model
+    poisson = Poisson()
+
+    # Create N sets of input
+    N = 10
+    f = torch.rand(N, 1, requires_grad=True, dtype=torch.float64)
+    g = torch.rand(N, 1, requires_grad=True, dtype=torch.float64)
+
+    # Solve the Poisson equation N times
+    u = poisson(f, g)
+
+    # Construct functional
+    J = u.sum()
+
+    # Execute the backward pass
+    J.backward()
+
+    # Extract gradients
+    dJdf: torch.Tensor = f.grad
+    dJdg: torch.Tensor = g.grad
+
+    print(dJdf, dJdg)
