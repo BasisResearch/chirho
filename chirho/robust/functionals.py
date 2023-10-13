fbaf7e06abc3b564c8159bb92d7b3c7796ccfdc4
Raj Agrawal <r.agrawal.mit@gmail.com>
2023-10-13 15:21:07 -0400

    eli's dice correction technique

--- chirho/robust/functionals.py
+++ chirho/robust/functionals.py
@@ -1,4 +1,5 @@
-from typing import Dict
+import functools
+from typing import Callable, Dict, ParamSpec, TypeVar
 
 import torch
 import pyro
@@ -9,10 +10,13 @@ from chirho.interventional.handlers import do
 from chirho.counterfactual.handlers import MultiWorldCounterfactual
 from chirho.indexed.ops import IndexSet, gather
 
+P = ParamSpec("P")
+T = TypeVar("T")
+
 
 def average_treatment_effect(
     model: PyroModule,
-    theta_hat: Dict[str, torch.tensor],
+    theta_hat: Dict[str, torch.Tensor],
     n_monte_carlo: int = 10000,
 ) -> torch.Tensor:
     """Compute the average treatment effect of a model."""
@@ -23,3 +27,29 @@ def average_treatment_effect(
         Y0 = gather(Ys, IndexSet(A={1}), event_dim=0)
         Y1 = gather(Ys, IndexSet(A={2}), event_dim=0)
     return pyro.deterministic("ATE", (Y1 - Y0).mean(dim=-1, keepdim=True).squeeze())
+
+
+Functional = Callable[[Callable[P, T], Dict[str, torch.Tensor], int], torch.Tensor]
+
+
+def dice_correction(functional: Functional[P, T]) -> Functional[P, T]:
+    @functools.wraps(functional)
+    def _corrected_functional(
+        model: Callable[P, T], theta_hat: Dict[str, torch.Tensor], n_monte_carlo: int
+    ) -> torch.Tensor:
+        with pyro.poutine.trace() as tr:
+            value: torch.Tensor = functional(model, theta_hat, n_monte_carlo)
+
+        tr.trace.compute_log_prob()
+        logp = torch.zeros_like(value)
+        for node in tr.trace.nodes.values():
+            if node["type"] != "sample" or pyro.poutine.util.site_is_subsample(node):
+                continue
+
+            if not node["is_observed"] and not node["fn"].has_rsample:
+                logp += node["log_prob"].sum()
+
+        dice_factor = torch.exp(logp - logp.detach())
+        return dice_factor * value
+
+    return _corrected_functional
