46c3256f7b9876da475e55fcd05e480010e977df
Eli <eli@elibingham.com>
2023-10-16 14:49:31 -0400

    remove inplacedynamics

--- tests/dynamical/dynamical_fixtures.py
+++ tests/dynamical/dynamical_fixtures.py
@@ -4,12 +4,12 @@ import pyro
 import torch
 from pyro.distributions import Normal, Uniform, constraints
 
-from chirho.dynamical.ops import InPlaceDynamics, State, Trajectory
+from chirho.dynamical.ops import State, Trajectory
 
 T = TypeVar("T")
 
 
-class UnifiedFixtureDynamics(InPlaceDynamics):
+class UnifiedFixtureDynamics:
     def __init__(self, beta=None, gamma=None):
         super().__init__()
 
