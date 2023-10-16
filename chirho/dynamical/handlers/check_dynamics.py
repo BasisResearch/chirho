842f12ab84f1e2111857b987dbadc3e83934f085
Sam Witty <samawitty@gmail.com>
2023-10-16 17:18:36 -0400

    effect handler for checking calls to simulate at runtime

---
+++ chirho/dynamical/handlers/check_dynamics.py
@@ -0,0 +1,17 @@
+
+import pyro
+from chirho.dynamical.internals.solver import check_dynamics, get_solver
+
+
+class RuntimeCheckDynamics(pyro.poutine.messenger.Messenger):
+    def _pyro_simulate(self, msg) -> None:
+        # Check whether the dynamics satisfy the assumptions of the solver.
+        dynamics, state, start_time, _ = msg["args"]
+
+        if msg["kwargs"].get("solver", None) is not None:
+            solver = msg["kwargs"]["solver"]
+        else:
+            solver = get_solver()
+
+        if not check_dynamics(solver, dynamics, state, start_time):
+            raise ValueError("Dynamics are not valid for the solver.")
