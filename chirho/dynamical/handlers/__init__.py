d6a465ece9094425b7b7be36d5977085bcbfb4ee
Sam Witty <samawitty@gmail.com>
2023-10-17 16:16:19 -0400

    add RuntimeCheckDynamics to init

--- chirho/dynamical/handlers/__init__.py
+++ chirho/dynamical/handlers/__init__.py
@@ -1,4 +1,5 @@
 from ..internals.solver import Solver  # noqa: F401
+from .check_dynamics import RuntimeCheckDynamics  # noqa: F401
 from .event_loop import InterruptionEventLoop  # noqa: F401
 from .interruption import (  # noqa: F401
     DynamicInterruption,
