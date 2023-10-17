2d27da5e3abea71c06e56f80b00863eb989821d4
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-10-17 20:40:42 +0200

    added backdoor.ipynb to tested notebooks

--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-INCLUDED_NOTEBOOKS="docs/source/tutorial_i.ipynb"
+INCLUDED_NOTEBOOKS="docs/source/tutorial_i.ipynb docs/source/backdoor.ipynb"
 
 CI=1 pytest --nbval-lax --dist loadscope -n auto $INCLUDED_NOTEBOOKS
