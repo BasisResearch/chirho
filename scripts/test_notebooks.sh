41b4c311b8e134b843ec34ecb816ce3da5e5e732
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-10-18 09:16:03 +0200

    added dr_learner

--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-INCLUDED_NOTEBOOKS="docs/source/tutorial_i.ipynb docs/source/backdoor.ipynb docs/source/cevae.ipynb"
+INCLUDED_NOTEBOOKS="docs/source/tutorial_i.ipynb docs/source/backdoor.ipynb docs/source/cevae.ipynb docs/source/dr_learner.ipynb"
 
 CI=1 pytest --nbval-lax --dist loadscope -n auto $INCLUDED_NOTEBOOKS
