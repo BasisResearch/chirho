647ed3e7827368196aec1f4a7d70e29a9c08a3cf
rfl-urbaniak <rfl.urbaniak@gmail.com>
2023-10-18 09:24:12 +0200

    added mediation.ipynb to testing

--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-INCLUDED_NOTEBOOKS="docs/source/tutorial_i.ipynb docs/source/backdoor.ipynb docs/source/cevae.ipynb docs/source/dr_learner.ipynb"
+INCLUDED_NOTEBOOKS="docs/source/tutorial_i.ipynb docs/source/backdoor.ipynb docs/source/cevae.ipynb 
+docs/source/dr_learner.ipynb docs/source/mediation.ipynb"
 
 CI=1 pytest --nbval-lax --dist loadscope -n auto $INCLUDED_NOTEBOOKS
