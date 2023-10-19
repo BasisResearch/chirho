3ac9ba8780f14773fb5e4fe5ac5b1cc2de016b35
Andy Zane <andy@basis.ai>
2023-10-19 15:11:01 -0600

    adds comparison to analytical solution of dome evolution, largely matching except that analytical solution appears to not be mass preserving.

--- docs/source/pde_scratch/halfar_ice.py
+++ docs/source/pde_scratch/halfar_ice.py
@@ -12,12 +12,29 @@ from torch import tensor as tt
 
 def initial_curve(x):
     condition = (x >= -1.0) & (x <= 1.0)
-    values = 1.0 - x**2 + 0.3 * torch.sin(2. * torch.pi * x) ** 2.
+    values = (1.0 - x**2 + 0.3 * torch.sin(2. * torch.pi * x) ** 2.)
     return torch.where(condition, values, torch.zeros_like(x))
 
 
 def fillna(tensor):
     return torch.where(torch.isnan(tensor), torch.zeros_like(tensor), tensor)
+
+
+def _t0(r0, h0, gamma=1.):
+    return (1. / (18. * gamma)) * ((7. / 4.) ** 3.) * (r0 ** 4. / h0 ** 7.)
+
+
+def halfar_ice_analytical(r, t, h0, r0, gamma=1.):
+    t0 = _t0(r0, h0, gamma)
+
+    hterm = (h0 * (t0 / t) ** (1. / 9.))
+    rterm = (1. - ((t0 / t) ** (1. / 18.) * (r / r0)) ** (4. / 3.)) ** (3. / 7.)
+
+    hhalf = hterm * rterm
+    rn = len(hhalf)
+    hhalf[:rn // 2] = torch.flip(hhalf[rn // 2:], dims=(0,))
+
+    return fillna(hhalf)
 
 
 class HalfarIce(torch_fenics.FEniCSModule):
@@ -60,11 +77,8 @@ class HalfarIce(torch_fenics.FEniCSModule):
             utr * vte * fe.dx
             - ult * vte * fe.dx
             + tstep * fe.dot(
-                # So the middle bit is usually grad(utr) * fe.sqrt(grad(utr) * grad(utr) + 1e-6) ** (n-1),
+                # So the middle bit is usually grad(utr) * fe.sqrt(grad(utr).dot(grad(utr)) + 1e-6) ** (n-1),
                 #  but that can be simplified for the typical n=3. Same with utr ** (n+2)
-                # TODO I'm not sure if, in general multi-dim case, that's an elementwise ^3 or if dotted.
-                #  For single dim doesn't matter though.
-                # fe.elem_pow(fe.grad(utr), fe.as_vector([3.])) * utr ** 5,
                 fe.grad(utr) * (ulk ** 5) * fe.dot(cont_grad_ulk, cont_grad_ulk),
                 fe.grad(vte)
             ) * fe.dx
@@ -89,13 +103,54 @@ class HalfarIce(torch_fenics.FEniCSModule):
         return fe.Constant(0), fe.Function(self.V), fe.Function(self.V)
 
 
+def anamain():
+    h0 = 1.
+    r0 = 1.
+
+    xx = torch.linspace(-2., 2., 512)
+
+    sols = []
+
+    t = 1.
+    tstep = 1.0
+    for i in range(1000):
+        h = halfar_ice_analytical(xx, t, h0, r0)
+        sols.append(h)
+        t += tstep
+
+    fig, ax = plt.subplots(dpi=500, figsize=(4, 2))
+    plot_sols(xx, sols, ax, skip=5)
+
+    # Sum across each solution to check if mass is being conserved.
+    # FIXME for some reason this was sigsegv-ing :/
+    # ice_sum = torch.stack(sols).sum(axis=-1)
+    ice_sum = [sol.sum() for sol in sols]
+    plt.figure()
+    plt.plot(ice_sum)
+
+
+def plot_sols(xx, sols, ax, skip=5, color1='purple', color2='orange', thickness1=1.0, thickness2=0.1):
+
+    ax.plot(xx, sols[0].detach(), color=color1, linestyle='--', label="Initial Condition", linewidth=thickness1)
+    for t_sols in sols[1:-1:skip]:
+        ax.plot(xx, t_sols.detach(), color=color2, alpha=0.5, linewidth=thickness2)
+    ax.plot(xx, sols[-1].detach(), color=color1, label="Final Solution", linewidth=thickness1)
+
+
 def main():
 
-    halfar_ice = HalfarIce(512)
+    halfar_ice = HalfarIce(511)
 
     xx = torch.linspace(-2., 2., halfar_ice.n_elements + 1)
-    u_init = initial_curve(xx)[None, :].double()\
-        .clone().detach().requires_grad_(True)  # Make this a leaf tensor.
+
+    # # A tricky initial glacier.
+    # u_init = initial_curve(xx)\
+    #     [None, :].double().clone().detach().requires_grad_(True)  # Make this a leaf tensor.
+
+    # Init with analytically known progression in time.
+    h0, r0 = 1., 1.
+    u_init = halfar_ice_analytical(r=xx, t=1., h0=h0, r0=r0)\
+        [None, :].double().clone().detach().requires_grad_(True)
 
     u_last_t = u_init
 
@@ -103,12 +158,13 @@ def main():
 
     sols = [u_last_t[0]]
 
-    tstep = tt(.0002).double()[None, None]
-    for i in range(500):
+    tstep = tt(0.005).double()[None, None]
+    end_t = 1.
+    for i in range(1000):
         if len(sols) > 1:
             outer_div = torch.linalg.norm(sols[-1] - sols[-2])
-            if outer_div < 1e-3:
-                break
+            if outer_div < 1e-6:
+                break  # break out if the glacier has reached equilibrium.
             outer_div = outer_div.item()
         else:
             outer_div = torch.inf
@@ -126,6 +182,7 @@ def main():
             min_div = div
             u_last_k = u_last_k_
         u_last_t = u_last_k
+        end_t += tstep.item()
         sols.append(u_last_t[0])
     print()
 
@@ -133,11 +190,8 @@ def main():
     #  Also see that initial condition is reversed.
     # sols = sols[:, :, ::-1]
 
-    plt.figure(dpi=500, figsize=(8, 2))
-    plt.plot(xx, sols[0].detach(), color='purple', linestyle='--', label="Initial Condition")
-    for t_sols in sols[1:-1:5]:
-        plt.plot(xx, t_sols.detach(), linewidth=0.1, color='orange', alpha=0.5)
-    plt.plot(xx, sols[-1].detach(), color='purple', label="Final Solution")
+    fig, ax = plt.subplots(dpi=500, figsize=(4, 2))
+    plot_sols(xx, sols, ax, skip=5)
 
     # remaining_heat = sols.sum(axis=-1)
     #
@@ -151,8 +205,7 @@ def main():
     #     diffusivities.grad.zero_()
     #     times.grad.zero_()
 
-    peak = sols[-1].max().detach()
-    analytical_solution_maybe = fillna(peak * (1. - torch.abs(xx))**.5)
+    analytical_solution_maybe = halfar_ice_analytical(xx, t=end_t, h0=h0, r0=r0)
     plt.plot(xx, analytical_solution_maybe, color='white', linestyle='--',
              linewidth=0.5, label="Analytical Equilibrium (Maybe)")
     plt.gca().set_aspect('equal')
@@ -173,5 +226,6 @@ def main():
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     main()
+    # anamain()
 
     plt.show()
