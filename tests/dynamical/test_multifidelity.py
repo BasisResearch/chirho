107e5b2f7d4f793feb39eb0eb86ef05a966146da
Eli <eli@elibingham.com>
2023-11-02 13:59:59 -0400

    logging instead of printing, parameters

--- tests/dynamical/test_multifidelity.py
+++ tests/dynamical/test_multifidelity.py
@@ -93,6 +93,7 @@ class LowFidelityShortColumn4(BaseShortColumn):
 
 
 @pytest.mark.parametrize("surrogate", [
+    HighFidelityShortColumn(cost=0.1),
     LowFidelityShortColumn2(cost=0.1),
     LowFidelityShortColumn3(cost=0.1),
     LowFidelityShortColumn4(cost=0.1),
@@ -102,45 +103,48 @@ class LowFidelityShortColumn4(BaseShortColumn):
 ])
 def test_shortcolumn_surrogates_correlation(surrogate):
 
+    num_samples = 1e6
+
     model = HighFidelityShortColumn(cost=1.)
 
-    with pyro.plate("samples", 1e6, dim=-1), pyro.poutine.trace() as tr:
+    with pyro.plate("samples", num_samples, dim=-1), pyro.poutine.trace() as tr:
         surrogate_samples = surrogate()
 
-    with pyro.plate("samples", 1e6, dim=-1):
+    with pyro.plate("samples", num_samples, dim=-1):
         high_fidelity_samples = pyro.poutine.replay(trace=tr.trace)(model)()
 
     assert not torch.any(torch.isnan(high_fidelity_samples))
-    assert not torch.any(torch.isnan(surrogate_samples))
-    print("high fidelity mean: ", high_fidelity_samples.mean(dim=-1))
-    print("surrogate mean: ", surrogate_samples.mean(dim=-1))
+    logging.info(f"high fidelity mean: {high_fidelity_samples.mean(dim=-1)}")
+    logging.info(f"high fidelity range: {high_fidelity_samples.min(dim=-1)[0], high_fidelity_samples.max(dim=-1)[0]}")
 
-    print("high fidelity range: ", high_fidelity_samples.min(dim=-1)[0], high_fidelity_samples.max(dim=-1)[0])
-    print("surrogate range: ", surrogate_samples.min(dim=-1)[0], surrogate_samples.max(dim=-1)[0])
+    assert not torch.any(torch.isnan(surrogate_samples))
+    logging.info(f"surrogate mean: {surrogate_samples.mean(dim=-1)}")
+    logging.info(f"surrogate range: {surrogate_samples.min(dim=-1)[0], surrogate_samples.max(dim=-1)[0]}")
 
     corr = scipy.stats.pearsonr(high_fidelity_samples.detach().numpy(), surrogate_samples.detach().numpy())
-    print("correlation: ", corr)
+    logging.info(f"correlation: {corr}")
     assert corr.statistic > 0.7
 
 
-@pytest.mark.parametrize("surrogate", [
-    LowFidelityShortColumn2(cost=0.1),
-    # LowFidelityShortColumn3(cost=0.1),
-    # LowFidelityShortColumn4(cost=0.1),
-    # LowFidelityShortColumn2(cost=1e-5),
-    # LowFidelityShortColumn3(cost=1e-5),
-    # LowFidelityShortColumn4(cost=1e-5),
+@pytest.mark.parametrize("surrogate,num_biasing_samples", [
+    (HighFidelityShortColumn(cost=0.1), 1e6),
+    (LowFidelityShortColumn2(cost=0.1), 1e6),
+    (LowFidelityShortColumn3(cost=0.1), 1e6),
+    (LowFidelityShortColumn4(cost=0.1), 1e6),
 ])
-def test_multifidelity_importance_1_surrogates_mean(surrogate):
+def test_multifidelity_importance_1_surrogates_mean(surrogate, num_biasing_samples):
+
+    num_true_samples = 1e6
+    num_train_steps = 2000
+    num_runs = 10
 
     model = HighFidelityShortColumn(cost=1.)
 
     # expected mean
-    with pyro.plate("samples", 1e6, dim=-1):
+    with pyro.plate("samples", num_true_samples, dim=-1):
         expected_result = model().mean(dim=-1)
- 
+
     actual_results = []
-    num_runs = 10
     for run in range(num_runs):
 
         # biasing distribution: draw samples from surrogate and fit a mixture model
@@ -157,35 +161,39 @@ def test_multifidelity_importance_1_surrogates_mean(surrogate):
         # fit surrogate guide
         with warnings.catch_warnings(), pyro.validation_enabled(False):
             warnings.filterwarnings("ignore", category=torch.jit.TracerWarning)
-            # surrogate_elbo = torch.jit.trace_module(surrogate_elbo, {"forward": ()}, strict=False, check_trace=False)
-            surrogate_elbo = torch.compile(surrogate_elbo, backend="inductor")
+            surrogate_elbo = torch.jit.trace_module(surrogate_elbo, {"forward": ()}, strict=False, check_trace=False)
         optim = torch.optim.Adam(surrogate_elbo.parameters(), lr=0.01)
-        for step in range(2000):
+        for step in range(num_train_steps):
             loss = surrogate_elbo()
             loss.backward()
             optim.step()
             optim.zero_grad()
             if step % 100 == 0:
-                print("run: ", run, "step: ", step, "loss: ", loss.item())
+                logging.info(f"run: {run}, step: {step}, loss: {loss.item()}")
 
         # importance
         log_weights, model_trace, surrogate_guide_trace = \
             pyro.infer.importance.vectorized_importance_weights(
                 pyro.poutine.block(hide=["log_column"])(reparam(model)),
                 surrogate_guide,
-                num_samples=1e6,
+                num_samples=num_biasing_samples,
                 max_plate_nesting=0,
             )
 
         result = torch.mean(log_weights.exp() * model_trace.nodes["column"]["value"], dim=-1)
-        print(result, torch.logsumexp(log_weights - log_weights.shape[-1], dim=-1).exp())
-        assert not torch.any(torch.isnan(result))
-        if not torch.isnan(result).any():
-            actual_results.append(result.item())
+        logging.info(f"{result}, {torch.logsumexp(log_weights - log_weights.shape[-1], dim=-1).exp()}")
 
-    print("expected high-fidelity mean: ", expected_result.item())
-    print("estimated multifidelity mean: ", torch.tensor(actual_results).mean().item())
-    print("mse: ", torch.mean((expected_result - torch.tensor(actual_results)) ** 2))
+        assert not torch.any(torch.isnan(result)), \
+            f"num weight nans: {torch.sum(torch.isnan(log_weights))} " + \
+            f"num fn nans: {torch.sum(torch.isnan(model_trace.nodes['column']['value']))}"
+        actual_results.append(result.item())
+
+    logging.info(f"expected high-fidelity mean: {expected_result.item()}")
+    logging.info(f"estimated multifidelity mean: {torch.tensor(actual_results).mean().item()}")
+
+    mse = torch.mean((expected_result - torch.tensor(actual_results)) ** 2)
+    logging.info(f"mse: {mse}")
+    assert mse < 1e-1
 
 
 def test_multifidelity_importance_n_surrogates():
