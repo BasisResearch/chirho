3f0c83d50ac9081f3414d79102b84df557d2bd63
Raj Agrawal <r.agrawal@csail.mit.edu>
2023-12-08 09:12:08 -0800

    Make `NMCLogPredictiveLikelihood` seeded (#408)
    * initial test against analytic fisher vp (pair coded w/ sam)
    
    * linting
    
    * added check against analytic ate
    
    * added vmap and grad smoke tests
    
    * added missing init
    
    * linting and consolidated fisher tests to one file
    
    * fixed types
    
    * fixing linting errors
    
    * trying to fix type error for python 3.8
    
    * fixing test errors
    
    * added patch to test to prevent from failing when denom is small
    
    * composition issue
    
    * seeded NMC implementation
    
    * linting
    
    * removed missing import
    
    * changed to eli's seedmessenger suggestion
    
    * added failing edge case
    
    * explicitly add max plate argument
    
    * added warning message
    
    * fixed linting error and test failure case from too many cg iters
    
    * eli's contextlib seeding strategy
    
    * removed seedmessenger from test
    
    * randomness should be shared across calls
    
    * switched back to different

--- tests/robust/test_internals_compositions.py
+++ tests/robust/test_internals_compositions.py
@@ -3,14 +3,13 @@ import warnings
 
 import pyro
 import torch
-from pyro.poutine.seed_messenger import SeedMessenger
 
 from chirho.robust.internals.linearize import (
     conjugate_gradient_solve,
     make_empirical_fisher_vp,
 )
 from chirho.robust.internals.predictive import NMCLogPredictiveLikelihood
-from chirho.robust.internals.utils import make_functional_call
+from chirho.robust.internals.utils import make_functional_call, reset_rng_state
 
 from .robust_fixtures import SimpleGuide, SimpleModel
 
@@ -23,14 +22,14 @@ def test_empirical_fisher_vp_nmclikelihood_cg_composition():
     model(), guide()  # initialize
     log_prob = NMCLogPredictiveLikelihood(model, guide, num_samples=100)
     log_prob_params, func_log_prob = make_functional_call(log_prob)
-    func_log_prob = SeedMessenger(123)(func_log_prob)
+    func_log_prob = reset_rng_state(pyro.util.get_rng_state())(func_log_prob)
 
     predictive = pyro.infer.Predictive(
         model, guide=guide, num_samples=1000, parallel=True, return_sites=["y"]
     )
     predictive_params, func_predictive = make_functional_call(predictive)
 
-    cg_solver = functools.partial(conjugate_gradient_solve, cg_iters=10)
+    cg_solver = functools.partial(conjugate_gradient_solve, cg_iters=2)
 
     with torch.no_grad():
         data = func_predictive(predictive_params)
@@ -59,3 +58,50 @@ def test_empirical_fisher_vp_nmclikelihood_cg_composition():
 
     assert torch.allclose(solve_one["guide.loc_a"], solve_two["guide.loc_a"])
     assert torch.allclose(solve_one["guide.loc_b"], solve_two["guide.loc_b"])
+
+
+def test_nmc_likelihood_seeded():
+    model = SimpleModel()
+    guide = SimpleGuide()
+    model(), guide()  # initialize
+
+    log_prob = NMCLogPredictiveLikelihood(
+        model, guide, num_samples=3, max_plate_nesting=3
+    )
+    log_prob_params, func_log_prob = make_functional_call(log_prob)
+
+    func_log_prob = reset_rng_state(pyro.util.get_rng_state())(func_log_prob)
+
+    datapoint = {"y": torch.tensor([1.0, 2.0, 3.0])}
+    prob_call_one = func_log_prob(log_prob_params, datapoint)
+    prob_call_two = func_log_prob(log_prob_params, datapoint)
+    prob_call_three = func_log_prob(log_prob_params, datapoint)
+    assert torch.allclose(prob_call_two, prob_call_three)
+    assert torch.allclose(prob_call_one, prob_call_two)
+
+    data = {"y": torch.tensor([[0.3665, 1.5440, 2.2210], [0.3665, 1.5440, 2.2210]])}
+
+    fvp = make_empirical_fisher_vp(func_log_prob, log_prob_params, data)
+
+    v = {k: torch.ones_like(v) for k, v in log_prob_params.items()}
+
+    assert fvp(v)["guide.loc_a"].abs().max() > 0
+    assert fvp(v)["guide.loc_b"].abs().max() > 0
+
+    # Check if fvp agrees across multiple calls of same `fvp` object
+    assert torch.allclose(fvp(v)["guide.loc_a"], fvp(v)["guide.loc_a"])
+    assert torch.allclose(fvp(v)["guide.loc_b"], fvp(v)["guide.loc_b"])
+
+    # Check if fvp agrees across different `fvp` objects with same inputs
+    fvp_one = make_empirical_fisher_vp(func_log_prob, log_prob_params, data)
+    fvp_two = make_empirical_fisher_vp(func_log_prob, log_prob_params, data)
+    assert torch.allclose(fvp_one(v)["guide.loc_a"], fvp_two(v)["guide.loc_a"])
+    assert torch.allclose(fvp_one(v)["guide.loc_b"], fvp_two(v)["guide.loc_b"])
+
+    # Since `data` has same datapoint twice, fvp with redundant
+    # data should agree when only the single datapoint is used if the
+    # seeding works correctly
+    data_two = {"y": torch.tensor([[0.3665, 1.5440, 2.2210]])}
+    fvp_two = make_empirical_fisher_vp(func_log_prob, log_prob_params, data_two)
+    assert torch.allclose(fvp_one(v)["guide.loc_a"], fvp_two(v)["guide.loc_a"])
+    assert torch.allclose(fvp_one(v)["guide.loc_b"], fvp_two(v)["guide.loc_b"])
