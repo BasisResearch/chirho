d09567bbfb5c93e12e67e01f19c8f9b7563ccd10
Sam Witty <samawitty@gmail.com>
2024-01-03 21:49:29 -0500

    added scaffolding to one step estimator

--- chirho/robust/handlers/estimators.py
+++ chirho/robust/handlers/estimators.py
@@ -9,6 +9,10 @@ def one_step_correction(
     model: Callable[P, Any],
     guide: Callable[P, Any],
     functional: Optional[Functional[P, S]] = None,
+    influence_fn_estimator: Callable[
+        [Callable[P, Any], Callable[P, Any], Optional[Functional[P, S]]],
+        Callable[Concatenate[Point[T], P], S],
+    ] = influence_fn,
     **influence_kwargs,
 ) -> Callable[Concatenate[Point[T], P], S]:
     """
@@ -23,6 +27,12 @@ def one_step_correction(
     :param functional: model summary of interest, which is a function of the
         model and guide. If ``None``, defaults to :class:`PredictiveFunctional`.
     :type functional: Optional[Functional[P, S]], optional
+    :param influence_fn_estimator: function to approximate the efficient influence
+        function. Defaults to :func:`influence_fn`.
+    :type influence_fn_estimator: Callable[
+        [Callable[P, Any], Callable[P, Any], Optional[Functional[P, S]]],
+        Callable[Concatenate[Point[T], P], S],
+    ]
     :return: function to compute the one-step correction
     :rtype: Callable[Concatenate[Point[T], P], S]
 
@@ -33,9 +43,51 @@ def one_step_correction(
     """
     influence_kwargs_one_step = influence_kwargs.copy()
     influence_kwargs_one_step["pointwise_influence"] = False
-    eif_fn = influence_fn(model, guide, functional, **influence_kwargs_one_step)
+    eif_fn = influence_fn_estimator(
+        model, guide, functional, **influence_kwargs_one_step
+    )
 
     def _one_step(test_data: Point[T], *args, **kwargs) -> S:
         return eif_fn(test_data, *args, **kwargs)
 
     return _one_step
+
+
+def one_step_corrected_estimator(
+    test_data: Point[T],
+    model: Callable[P, Any],
+    guide: Callable[P, Any],
+    functional: Optional[Functional[P, S]] = None,
+    influence_fn_estimator=influence_fn,
+    **influence_kwargs,
+) -> S:
+    """
+    Returns the one-step corrected estimator for the functional at a
+    specified set of test points as discussed in [1].
+
+    :param test_data: test data
+    :type test_data: Point[T]
+    :param model: Python callable containing Pyro primitives.
+    :type model: Callable[P, Any]
+    :param guide: Python callable containing Pyro primitives.
+        Must only contain continuous latent variables.
+    :type guide: Callable[P, Any]
+    :param functional: model summary of interest, which is a function of the
+        model and guide. If ``None``, defaults to :class:`PredictiveFunctional`.
+    :type functional: Optional[Functional[P, S]], optional
+    :param influence_fn_estimator: function to approximate the efficient influence
+        function. Defaults to :func:`influence_fn`.
+    :type influence_fn_estimator: Callable[
+        [Callable[P, Any], Callable[P, Any], Optional[Functional[P, S]]],
+        Callable[Concatenate[Point[T], P], S],
+    ]
+    :return: the one-step corrected estimator for the functional evaluated at the
+        test points.
+    :rtype: S
+    """
+    plug_in_estimate = functional(model, guide)()
+    correction = one_step_correction(
+        model, guide, functional, influence_fn_estimator, **influence_kwargs
+    )(test_data)
+    
+    return plug_in_estimate + correction
