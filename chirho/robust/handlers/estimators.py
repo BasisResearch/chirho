975114b1043707c217a58126a8b0fb5678e7c397
Sam Witty <samawitty@gmail.com>
2024-01-04 14:50:53 -0500

    replace dispatch with pytree flatten and unflatten

--- chirho/robust/handlers/estimators.py
+++ chirho/robust/handlers/estimators.py
@@ -1,4 +1,3 @@
-from functools import singledispatch
 from typing import Any, Callable, Optional
 
 import torch
@@ -100,30 +99,13 @@ def one_step_corrected_estimator(
     def _one_step_corrected_estimator(test_data: Point[T], *args, **kwargs) -> S:
         plug_in_estimate = plug_in_estimator(*args, **kwargs)
         correction = correction_estimator(test_data, *args, **kwargs)
-        return apply_correction(plug_in_estimate, correction)
+
+        flat_plug_in_estimate, treespec = torch.utils._pytree.tree_flatten(
+            plug_in_estimate
+        )
+        flat_correction, _ = torch.utils._pytree.tree_flatten(correction)
+        return torch.utils._pytree.tree_unflatten(
+            [a + b for a, b in zip(flat_plug_in_estimate, flat_correction)], treespec
+        )
 
     return _one_step_corrected_estimator
-
-
-@singledispatch
-def apply_correction(plug_in_estimate, correction: S) -> S:
-    raise NotImplementedError(
-        "The functional must produce a function that return a torch Tensor or a dict of torch Tensors"
-    )
-
-
-@apply_correction.register(dict)
-def apply_correction_point(
-    plug_in_estimate: Point[T], correction: Point[T]
-) -> Point[T]:
-    assert isinstance(correction, dict)
-    assert set(plug_in_estimate.keys()) == set(correction.keys())
-    return {k: plug_in_estimate[k] + correction[k] for k in plug_in_estimate.keys()}
-
-
-@apply_correction.register(torch.Tensor)
-def apply_correction_tensor(
-    plug_in_estimate: torch.Tensor, correction: torch.Tensor
-) -> torch.Tensor:
-    assert isinstance(correction, torch.Tensor)
-    return plug_in_estimate + correction
