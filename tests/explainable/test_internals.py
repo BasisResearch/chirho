4fb26017c821ad5deb9af4fcc7fc8cda4a5bbdcb
Eli <eli@elibingham.com>
2024-01-08 11:34:01 -0500

    remove test_defaults

--- tests/explainable/test_internals.py
+++ tests/explainable/test_internals.py
@@ -1,9 +1,31 @@
 import math
 
 import pyro.distributions.constraints as constraints
+import pytest
 import torch
 
-from chirho.explainable.internals.defaults import soft_eq, soft_neq
+from chirho.explainable.internals.defaults import soft_eq, soft_neq, uniform_proposal
+
+SUPPORT_CASES = [
+    constraints.real,
+    constraints.boolean,
+    constraints.positive,
+    constraints.interval(0, 10),
+    constraints.interval(-5, 5),
+    constraints.integer_interval(0, 2),
+    constraints.integer_interval(0, 100),
+]
+
+
+@pytest.mark.parametrize("support", SUPPORT_CASES)
+@pytest.mark.parametrize("event_shape", [(), (3,), (3, 2)], ids=str)
+def test_uniform_proposal(support, event_shape):
+    if event_shape:
+        support = constraints.independent(support, len(event_shape))
+
+    uniform = uniform_proposal(support, event_shape=event_shape)
+    samples = uniform.sample((10,))
+    assert torch.all(support.check(samples))
 
 
 def test_soft_boolean():
