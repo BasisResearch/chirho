f14f8191db143b80b9b2be149dea7989a68998c8
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-01-10 19:39:01 +0100

    add causal explanation to tests

--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-INCLUDED_NOTEBOOKS="docs/source/tutorial_i.ipynb docs/source/backdoor.ipynb docs/source/dr_learner.ipynb docs/source/mediation.ipynb docs/source/sdid.ipynb docs/source/slc.ipynb docs/source/dynamical_intro.ipynb docs/source/actual_causality.ipynb docs/source/causal_explanation.ipynb"
+INCLUDED_NOTEBOOKS="docs/source/tutorial_i.ipynb docs/source/backdoor.ipynb docs/source/dr_learner.ipynb docs/source/mediation.ipynb docs/source/sdid.ipynb docs/source/slc.ipynb docs/source/dynamical_intro.ipynb docs/source/actual_causality.ipynb docs/source/causal_explanation.ipynb docs/source/causal_explanation.ipynb"
 
 CI=1 pytest --nbval-lax --dist loadscope -n auto $INCLUDED_NOTEBOOKS
