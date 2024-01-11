3ea42748a29c550b5d8d332ec8047de1be3f66ed
Eli <eli@elibingham.com>
2024-01-11 09:53:22 -0500

    docstring

--- chirho/robust/handlers/estimators.py
+++ chirho/robust/handlers/estimators.py
@@ -11,15 +11,15 @@ T = TypeVar("T")
 
 def one_step_correction(
     functional: Functional[P, S],
-    test_data: Point[T],
+    *test_points: Point[T],
     **influence_kwargs,
 ) -> Functional[P, S]:
     """
-    Returns a function that computes the one-step correction for the
-    functional at a specified set of test points as discussed in
-    [1].
+    Returns a functional that computes the one-step correction for the
+    functional at a specified set of test points as discussed in [1].
 
-    :param functional: model summary of interest, which is a function of the model.
+    :param functional: model summary functional of interest
+    :param test_points: points at which to compute the one-step correction
     :return: functional to compute the one-step correction
 
     **References**
@@ -29,4 +29,4 @@ def one_step_correction(
     """
     influence_kwargs_one_step = influence_kwargs.copy()
     influence_kwargs_one_step["pointwise_influence"] = False
-    return influence_fn(functional, test_data, **influence_kwargs_one_step)
+    return influence_fn(functional, *test_points, **influence_kwargs_one_step)
