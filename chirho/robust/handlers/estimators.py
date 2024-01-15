da108e7c4867224cd372fd977c99730ed2f884c9
Sam Witty <samawitty@gmail.com>
2024-01-15 14:47:22 -0500

    working end-to-end tmle

--- chirho/robust/handlers/estimators.py
+++ chirho/robust/handlers/estimators.py
@@ -1,8 +1,6 @@
 import copy
-import pdb
 from typing import Any, Callable, TypeVar
 
-import pyro
 import torch
 from typing_extensions import ParamSpec
 
@@ -13,6 +11,40 @@ from chirho.robust.ops import Functional, Point, influence_fn
 P = ParamSpec("P")
 S = TypeVar("S")
 T = TypeVar("T")
+
+
+def tmle_scipy_optimize_wrapper(
+    packed_influence, log_jitter: float = 1e-6
+) -> torch.Tensor:
+    import numpy as np
+    import scipy
+    from scipy.optimize import LinearConstraint
+
+    # Turn things into numpy. This makes us sad... :(
+    D = packed_influence.detach().numpy()
+
+    N, L = D.shape[0], D.shape[1]
+
+    def loss(epsilon):
+        correction = 1 + D.dot(epsilon)
+
+        return np.sum(np.log(np.maximum(correction, log_jitter)))
+
+    positive_density_constraint = LinearConstraint(
+        D, -1 * np.ones(N), np.inf * np.ones(N)
+    )
+
+    epsilon_solve = scipy.optimize.minimize(
+        loss, np.zeros(L, dtype=D.dtype), constraints=positive_density_constraint
+    )
+
+    if not epsilon_solve.success:
+        raise RuntimeError("TMLE optimization did not converge.")
+
+    # Convert epsilon back to torch. This makes us happy... :)
+    packed_epsilon = torch.tensor(epsilon_solve.x, dtype=packed_influence.dtype)
+
+    return packed_epsilon
 
 
 def tmle(
@@ -28,46 +60,20 @@ def tmle(
 ) -> Functional[P, S]:
     from chirho.robust.internals.nmc import BatchedNMCLogMarginalLikelihood
 
-    def tmle_scipy_optimize_wrapper(packed_influence) -> torch.Tensor:
-        import numpy as np
-        import scipy
-        from scipy.optimize import LinearConstraint
-        # Turn things into numpy. This makes us sad... :(
-        D = packed_influence.detach().numpy()
-
-        N, L = D.shape[0], D.shape[1]
-
-        def loss(epsilon):
-            correction = 1 + D.dot(epsilon)
-
-            return np.sum(np.log(np.maximum(correction, log_jitter)))
-        
-        positive_density_constraint = LinearConstraint(D, -1 * np.ones(N), np.inf * np.ones(N))
-
-        epsilon_solve = scipy.optimize.minimize(loss, np.zeros(L, dtype=D.dtype), constraints=positive_density_constraint)
-
-        if not epsilon_solve.success:
-            raise RuntimeError("TMLE optimization did not converge.")
-        
-        # Convert epsilon back to torch. This makes us happy... :)
-        packed_epsilon = torch.tensor(epsilon_solve.x, dtype=packed_influence.dtype)
-
-        return packed_epsilon
-
-    def _solve_epsilon(prev_model: Callable[P, Any], *args, **kwargs) -> torch.Tensor:
+    def _solve_epsilon(prev_model: torch.nn.Module, *args, **kwargs) -> torch.Tensor:
         # find epsilon that minimizes the corrected density on test data
 
         influence_at_test = influence_fn(functional, test_point, **influence_kwargs)(
             prev_model
         )(*args, **kwargs)
 
-        flat_influence_at_test, _ = torch.utils._pytree.tree_flatten(
-            influence_at_test
-        )
+        flat_influence_at_test, _ = torch.utils._pytree.tree_flatten(influence_at_test)
 
         N = flat_influence_at_test[0].shape[0]
 
-        packed_influence_at_test = torch.concatenate([i.reshape(N, -1) for i in flat_influence_at_test])
+        packed_influence_at_test = torch.concatenate(
+            [i.reshape(N, -1) for i in flat_influence_at_test]
+        )
 
         packed_epsilon = tmle_scipy_optimize_wrapper(packed_influence_at_test)
 
@@ -75,38 +81,54 @@ def tmle(
 
     def _solve_model_projection(
         packed_epsilon: torch.Tensor,
-        prev_model: Callable[P, Any],
+        prev_model: torch.nn.Module,
         *args,
         **kwargs,
-    ) -> Callable[P, Any]:
-        
-        batched_log_prob = BatchedNMCLogMarginalLikelihood(
+    ) -> torch.nn.Module:
+        batched_log_prob: torch.nn.Module = BatchedNMCLogMarginalLikelihood(
             prev_model, num_samples=num_nmc_samples
         )
         prev_params, log_p_phi_pointwise = make_functional_call(batched_log_prob)
         prev_params = {k: v.detach() for k, v in prev_params.items()}
-        
+
         def log_p_phi(*args, **kwargs) -> torch.Tensor:
             return torch.sum(log_p_phi_pointwise(*args, **kwargs))
-        
-        _, functional_model = make_functional_call(PredictiveFunctional(prev_model, num_samples=num_grad_samples))
+
+        _, functional_model = make_functional_call(
+            PredictiveFunctional(prev_model, num_samples=num_grad_samples)
+        )
 
         new_params = copy.deepcopy(prev_params)
 
         def log_p_epsilon(params, x, *args, **kwargs):
-            influence = influence_fn(functional, x, **influence_kwargs)(prev_model)(*args, **kwargs)
+            influence = influence_fn(functional, x, **influence_kwargs)(prev_model)(
+                *args, **kwargs
+            )
             flat_influence, _ = torch.utils._pytree.tree_flatten(influence)
             N_x = flat_influence[0].shape[0]
 
-            packed_influence = torch.concatenate([i.reshape(N_x, -1) for i in flat_influence]).detach()
+            packed_influence = torch.concatenate(
+                [i.reshape(N_x, -1) for i in flat_influence]
+            ).detach()
 
-            log_likelihood_correction = torch.sum(torch.log(torch.maximum(1 + packed_influence.mv(packed_epsilon), torch.tensor(log_jitter))))
+            log_likelihood_correction = torch.sum(
+                torch.log(
+                    torch.maximum(
+                        1 + packed_influence.mv(packed_epsilon),
+                        torch.tensor(log_jitter),
+                    )
+                )
+            )
 
             return log_likelihood_correction + log_p_phi(params, x, *args, **kwargs)
 
         def loss(new_params, *args, **kwargs):
             # Sample data from the variational approximation
-            samples = {k: v for k, v in functional_model(new_params, *args, **kwargs).items() if k in test_point}
+            samples = {
+                k: v
+                for k, v in functional_model(new_params, *args, **kwargs).items()
+                if k in test_point
+            }
             term1 = log_p_phi(new_params, samples, *args, **kwargs)
             term2 = log_p_epsilon(new_params, samples, *args, **kwargs)
 
@@ -115,35 +137,38 @@ def tmle(
         grad_fn = torch.func.grad(loss)
 
         for _ in range(n_steps):
-            print(new_params, prev_params, loss(new_params, *args, **kwargs))
             grad = grad_fn(new_params, *args, **kwargs)
 
-            new_params = {k: (v - learning_rate * grad[k]) for k, v in new_params.items()}
+            new_params = {
+                k: (v - learning_rate * grad[k]) for k, v in new_params.items()
+            }
 
-        # TODO: pack new_params into a callable
+        for parameter_name, parameter in prev_model.named_parameters():
+            parameter.data = new_params[f"model.{parameter_name}"]
 
-        return new_model
+        return prev_model
 
-    def _one_step(prev_model: Callable[P, Any], *args, **kwargs) -> Callable[P, Any]:
-        # TODO: assert that this does not have side effects on prev_model
-        packed_epsilon = _solve_epsilon(prev_model, *args, **kwargs)
+    def _corrected_functional(*models: Callable[P, Any]) -> Callable[P, S]:
+        assert len(models) == 1
+        model = models[0]
 
-        new_model = _solve_model_projection(packed_epsilon, prev_model, *args, **kwargs)
+        assert isinstance(model, torch.nn.Module)
 
-        return new_model
-
-    def _tmle(model: Callable[P, Any]) -> S:
-        def _tmle_inner(*args, **kwargs):
-            tmle_model = model
+        def _estimator(*args, **kwargs) -> S:
+            tmle_model = copy.deepcopy(model)
 
             for _ in range(n_tmle_steps):
-                tmle_model = _one_step(tmle_model, *args, **kwargs)
+                packed_epsilon = _solve_epsilon(tmle_model, *args, **kwargs)
+
+                tmle_model = _solve_model_projection(
+                    packed_epsilon, tmle_model, *args, **kwargs
+                )
 
             return functional(tmle_model)(*args, **kwargs)
 
-        return _tmle_inner
+        return _estimator
 
-    return _tmle
+    return _corrected_functional
 
 
 def one_step_corrected_estimator(
