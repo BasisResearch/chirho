960b111515f25ea3572692f524d4085010e84c29
Raj Agrawal <r.agrawal.mit@gmail.com>
2024-01-17 13:18:16 -0500

    causal glm

--- docs/examples/robust_paper/models.py
+++ docs/examples/robust_paper/models.py
@@ -1,8 +1,143 @@
-class causal_GLM:
-    pass
+from typing import Callable, Optional
+import torch
+import math
+import pyro
+import pyro.distributions as dist
+
+pyro.settings.set(module_local_params=True)
+
+
+class CausalGLM(pyro.nn.PyroModule):
+    def __init__(
+        self,
+        p: int,
+        link_fn: Callable[..., dist.Distribution] = lambda mu: dist.Normal(mu, 1.0),
+        prior_scale: Optional[float] = None,
+    ):
+        super().__init__()
+        self.p = p
+        self.link_fn = link_fn
+        if prior_scale is None:
+            self.prior_scale = 1 / math.sqrt(self.p)
+        else:
+            self.prior_scale = prior_scale
+
+    def sample_outcome_weights(self):
+        return pyro.sample(
+            "outcome_weights",
+            dist.Normal(0.0, self.prior_scale).expand((self.p,)).to_event(1),
+        )
+
+    def sample_intercept(self):
+        return pyro.sample("intercept", dist.Normal(0.0, 1.0))
+
+    def sample_propensity_weights(self):
+        return pyro.sample(
+            "propensity_weights",
+            dist.Normal(0.0, self.prior_scale).expand((self.p,)).to_event(1),
+        )
+
+    def sample_treatment_weight(self):
+        return pyro.sample("treatment_weight", dist.Normal(0.0, 1.0))
+
+    def sample_covariate_loc_scale(self):
+        return torch.zeros(self.p), torch.ones(self.p)
+
+    def forward(self):
+        intercept = self.sample_intercept()
+        outcome_weights = self.sample_outcome_weights()
+        propensity_weights = self.sample_propensity_weights()
+        tau = self.sample_treatment_weight()
+        x_loc, x_scale = self.sample_covariate_loc_scale()
+        X = pyro.sample("X", dist.Normal(x_loc, x_scale).to_event(1))
+        A = pyro.sample(
+            "A",
+            dist.Bernoulli(
+                logits=torch.einsum("...i,...i->...", X, propensity_weights)
+            ),
+        )
+
+        return pyro.sample(
+            "Y",
+            self.link_fn(
+                torch.einsum("...i,...i->...", X, outcome_weights) + A * tau + intercept
+            ),
+        )
+
+
+class ConditionedCausalGLM(CausalGLM):
+    def __init__(
+        self,
+        X: torch.Tensor,
+        A: torch.Tensor,
+        Y: torch.Tensor,
+        link_fn: Callable[..., dist.Distribution] = lambda mu: dist.Normal(mu, 1.0),
+        prior_scale: Optional[float] = None,
+    ):
+        p = X.shape[1]
+        super().__init__(p, link_fn, prior_scale)
+        self.X = X
+        self.A = A
+        self.Y = Y
+
+    def forward(self):
+        intercept = self.sample_intercept()
+        outcome_weights = self.sample_outcome_weights()
+        propensity_weights = self.sample_propensity_weights()
+        tau = self.sample_treatment_weight()
+        x_loc, x_scale = self.sample_covariate_loc_scale()
+        with pyro.plate("__train__", size=self.X.shape[0], dim=-1):
+            X = pyro.sample("X", dist.Normal(x_loc, x_scale).to_event(1), obs=self.X)
+            A = pyro.sample(
+                "A",
+                dist.Bernoulli(
+                    logits=torch.einsum("ni,i->n", self.X, propensity_weights)
+                ),
+                obs=self.A,
+            )
+            pyro.sample(
+                "Y",
+                self.link_fn(
+                    torch.einsum("ni,i->n", X, outcome_weights) + A * tau + intercept
+                ),
+                obs=self.Y,
+            )
+
+
+class DataGeneratorCausalGLM(CausalGLM):
+    def __init__(
+        self,
+        p: int,
+        alpha: int,
+        beta: int,
+        link_fn: Callable[..., dist.Distribution] = lambda mu: dist.Normal(mu, 1.0),
+        treatment_weight: float = 0.0,
+    ):
+        super().__init__(p, link_fn)
+        self.alpha = alpha  # sparsity of propensity weights
+        self.beta = beta  # sparsity of outcome weights
+        self.treatment_weight = treatment_weight
+
+    def sample_outcome_weights(self):
+        outcome_weights = 1 / math.sqrt(self.beta) * torch.ones(self.p)
+        outcome_weights[self.beta :] = 0.0
+        return outcome_weights
+
+    def sample_propensity_weights(self):
+        propensity_weights = 1 / math.sqrt(self.alpha) * torch.ones(self.p)
+        propensity_weights[self.alpha :] = 0.0
+        return propensity_weights
+
+    def sample_treatment_weight(self):
+        return torch.tensor(self.treatment_weight)
+
+    def sample_intercept(self):
+        return torch.tensor(0.0)
+
 
 class kernel_ridge:
     pass
+
 
 class neural_network:
     pass
