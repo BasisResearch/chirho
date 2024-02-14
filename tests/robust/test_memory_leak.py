a3c6c96cfc332f4c7f1d96dfd4c93a957941923b
Raj Agrawal <r.agrawal.mit@gmail.com>
2024-02-13 23:19:36 -0500

    added explicit memory test

--- tests/robust/test_memory_leak.py
+++ tests/robust/test_memory_leak.py
@@ -1,3 +1,5 @@
+import math
+
 import psutil
 import pyro
 import pyro.distributions as dist
@@ -77,15 +79,16 @@ def humansize(nbytes):
     return "%s %s" % (f, suffixes[i])
 
 
-N_pts = 500
-mu_true = 0.0
-sd_true = 1.0
-true_model = GroundTruthToyNormal(mu_true, sd_true)
-D_pts = Predictive(true_model, num_samples=N_pts, return_sites=["Y"])()
-Y_pts = D_pts["Y"]
-Y_pts = torch.sort(Y_pts).values
+def test_linearize_does_not_leak_memory_new_interface():
+    N_pts = 500
+    mu_true = 0.0
+    sd_true = 1.0
+    true_model = GroundTruthToyNormal(mu_true, sd_true)
+    D_pts = Predictive(true_model, num_samples=N_pts, return_sites=["Y"])()
+    Y_pts = D_pts["Y"]
+    Y_pts = torch.sort(Y_pts).values
 
-for detach in [True, False]:
+    free_memory = []
     for i in range(25):
         mem = psutil.virtual_memory()
 
@@ -101,8 +104,47 @@ for detach in [True, False]:
             PredictiveModel(model, guide),
             num_samples_outer=10000,
             num_samples_inner=1,
-            detach=detach,
+            detach=True,
         )({"Y": Y_pts})
+        free_memory.append(mem.free)
         memory_size = humansize(mem.free)
-        print(f"Iteration: {i}, Detached: {detach}, Free Memory: {memory_size}")
-    print("\n")
+        print(f"Iteration: {i}, Detached: {True}, Free Memory: {memory_size}")
+
+    # Free memory should not be more than 10x different than the initial free memory
+    assert math.fabs((free_memory[-1] - free_memory[1])) / free_memory[1] < 0.1
+
+
+def test_linearize_does_not_leak_memory_no_grad():
+    N_pts = 500
+    mu_true = 0.0
+    sd_true = 1.0
+    true_model = GroundTruthToyNormal(mu_true, sd_true)
+    D_pts = Predictive(true_model, num_samples=N_pts, return_sites=["Y"])()
+    Y_pts = D_pts["Y"]
+    Y_pts = torch.sort(Y_pts).values
+
+    free_memory = []
+    with torch.no_grad():
+        for i in range(25):
+            mem = psutil.virtual_memory()
+
+            theta_true = {
+                "mu": torch.tensor(mu_true, requires_grad=True),
+                "sd": torch.tensor(sd_true, requires_grad=True),
+            }
+            model = ToyNormal()
+            guide = MLEGuide(theta_true)
+
+            # Linearize model
+            monte_eif = linearize(
+                PredictiveModel(model, guide),
+                num_samples_outer=10000,
+                num_samples_inner=1,
+                detach=False,
+            )({"Y": Y_pts})
+            free_memory.append(mem.free)
+            memory_size = humansize(mem.free)
+            print(f"Iteration: {i}, Detached: {True}, Free Memory: {memory_size}")
+
+    # Free memory should not be more than 10x different than the initial free memory
+    assert math.fabs((free_memory[-1] - free_memory[1])) / free_memory[1] < 0.1
