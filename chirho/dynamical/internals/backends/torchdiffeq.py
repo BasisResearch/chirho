c01785cc05e02cff398d800ec93348be101d43a9
Sam Witty <samawitty@gmail.com>
2024-02-21 20:29:56 -0500

    revert commit to show plate test failing

--- chirho/dynamical/internals/backends/torchdiffeq.py
+++ chirho/dynamical/internals/backends/torchdiffeq.py
@@ -105,11 +105,8 @@ def _batched_odeint(
     # TODO support event_dim > 0
     event_dim = 0  # assume states are batches of values of rank event_dim
 
-    # Call deriv once to get the shape of the output.
-    dy0 = func(t[..., 0], y0)
-
     y0_batch_shape = torch.broadcast_shapes(
-        *(dy0_.shape[: len(dy0_.shape) - event_dim] for dy0_ in dy0)
+        *(y0_.shape[: len(y0_.shape) - event_dim] for y0_ in y0)
     )
 
     y0_expanded = tuple(
