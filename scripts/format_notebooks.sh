a1b926c2ab27bf75c523e9cad9e41bae70057f13
rfl-urbaniak <rfl.urbaniak@gmail.com>
2024-04-04 09:28:36 +0200

    delete redundant script

--- scripts/format_notebooks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-INCLUDED_NOTEBOOKS="docs/source/tutorial_i.ipynb docs/source/backdoor.ipynb docs/source/dr_learner.ipynb docs/source/mediation.ipynb docs/source/sdid.ipynb docs/source/slc.ipynb docs/source/dynamical_intro.ipynb docs/source/actual_causality.ipynb"
