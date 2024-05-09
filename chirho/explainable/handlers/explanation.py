2b180057a8226a0e358e01f581b9d368f3e413a9
Eli <eli@elibingham.com>
2024-05-09 10:14:10 -0400

    flatten

--- chirho/explainable/handlers/explanation.py
+++ chirho/explainable/handlers/explanation.py
@@ -50,20 +50,24 @@ def SplitSubsets(
             yield
 
 
+@contextlib.contextmanager
 def ExplanationTransform(
     supports: Mapping[str, constraints.Constraint],
+    antecedents: Mapping[str, Optional[Observation[S]]],
+    consequents: Mapping[str, Optional[Observation[T]]],
+    witnesses: Optional[
+        Mapping[str, Optional[Union[Observation[S], Observation[T]]]]
+    ] = None,
+    *,
     antecedent_alternatives: Optional[Mapping[str, Intervention[S]]] = None,
     consequent_factors: Optional[Mapping[str, Callable[[T], torch.Tensor]]] = None,
     witness_preemptions: Optional[
         Mapping[str, Union[Intervention[S], Intervention[T]]]
     ] = None,
-    *,
-    antecedent_bias: float = 0.0,
     consequent_scale: float = 1e-2,
+    antecedent_bias: float = 0.0,
     witness_bias: float = 0.0,
-    antecedent_prefix: str = "__antecedent_",
-    witness_prefix: str = "__witness_",
-    consequent_prefix: str = "__consequent_",
+    prefix: str = "__cause_",
 ):
     """
     A context manager used for a stochastic search of minimal but-for causes among potential interventions.
@@ -73,6 +77,9 @@ def ExplanationTransform(
     intervention is executed. See tests in `tests/explainable/test_handlers_explanation.py` for examples.
 
     :param supports: A mapping of sites to their support constraints.
+    :param antecedents: A mapping of antecedent names to observations.
+    :param consequents: A mapping of consequent names to observations.
+    :param witnesses: A mapping of witness names to observations.
     :param antecedent_alternatives: A mapping of antecedent names to interventions.
     :param consequent_factors: A mapping of consequent names to factor functions.
     :param witness_preemptions: A mapping of witness names to interventions.
@@ -85,92 +92,79 @@ def ExplanationTransform(
 
     :return: A context manager that can be used to query the evidence.
     """
+    assert len(antecedents) > 0
+    assert len(consequents) > 0
+    assert not set(consequents.keys()) & set(antecedents.keys())
+    assert set(antecedents.keys()) <= set(supports.keys())
+    assert set(consequents.keys()) <= set(supports.keys())
+    if witnesses is not None:
+        assert set(witnesses.keys()) <= set(supports.keys())
+        assert not set(witnesses.keys()) & set(consequents.keys())
 
-    @contextlib.contextmanager
-    def _query(
-        antecedents: Mapping[str, Optional[Observation[S]]],
-        consequents: Mapping[str, Optional[Observation[T]]],
-        witnesses: Optional[
-            Mapping[str, Optional[Union[Observation[S], Observation[T]]]]
-        ] = None,
-    ):
-        assert len(antecedents) > 0
-        assert len(consequents) > 0
-        assert not set(consequents.keys()) & set(antecedents.keys())
-        assert set(antecedents.keys()) <= set(supports.keys())
-        assert set(consequents.keys()) <= set(supports.keys())
-        if witnesses is not None:
-            assert set(witnesses.keys()) <= set(supports.keys())
-            assert not set(witnesses.keys()) & set(consequents.keys())
-
-        # default argument values
-        if antecedent_alternatives is None:
-            necessity_interventions = {
-                a: random_intervention(supports[a], name=f"__antecedent_proposal_{a}")
-                for a in antecedents.keys()
-            }
-        else:
-            necessity_interventions = {
-                a: antecedent_alternatives[a] for a in antecedents.keys()
-            }
-
-        sufficiency_interventions = {
-            a: (
-                aa
-                if aa is not None
-                else sufficiency_intervention(
-                    supports[a], antecedents=antecedents.keys()
-                )
-            )
-            for a, aa in antecedents.items()
-        }
-
-        interventions = {
-            a: (necessity_interventions[a], sufficiency_interventions[a])
+    # default argument values
+    if antecedent_alternatives is None:
+        necessity_interventions = {
+            a: random_intervention(supports[a], name=f"{prefix}__alternative_{a}")
             for a in antecedents.keys()
         }
+    else:
+        necessity_interventions = {
+            a: antecedent_alternatives[a] for a in antecedents.keys()
+        }
 
-        if consequent_factors is None:
-            factors = {
-                c: consequent_eq_neq(
-                    support=supports[c],
-                    antecedents=antecedents.keys(),
-                    scale=consequent_scale,
-                )
-                for c in consequents.keys()
-            }
-        else:
-            factors = {c: consequent_factors[c] for c in consequents.keys()}
-
-        if witnesses is None:
-            witness_vars = set(supports.keys()) - set(consequents.keys())
-        else:
-            witness_vars = set(witnesses.keys())
-
-        if witness_preemptions is None:
-            preemptions = {
-                w: undo_split(supports[w], antecedents=antecedents.keys())
-                for w in witness_vars
-            }
-        else:
-            preemptions = {w: witness_preemptions[w] for w in witness_vars}
-
-        antecedent_handler = SplitSubsets(
-            {a: supports[a] for a in antecedents.keys()},
-            interventions,
-            bias=antecedent_bias,
-            prefix=antecedent_prefix,
+    sufficiency_interventions = {
+        a: (
+            aa
+            if aa is not None
+            else sufficiency_intervention(supports[a], antecedents=antecedents.keys())
         )
-        consequent_handler = Factors(factors, prefix=consequent_prefix)
-        witness_handler = Preemptions(
-            preemptions, bias=witness_bias, prefix=witness_prefix
-        )
-        with antecedent_handler, witness_handler, consequent_handler:
-            evidence: Mapping[str, Union[Observation[S], Observation[T]]] = {
-                **{a: aa for a, aa in antecedents.items() if aa is not None},
-                **{c: cc for c, cc in consequents.items() if cc is not None},
-                **{w: ww for w, ww in (witnesses or {}).items() if ww is not None},
-            }
-            yield evidence
+        for a, aa in antecedents.items()
+    }
 
-    return _query
+    interventions = {
+        a: (necessity_interventions[a], sufficiency_interventions[a])
+        for a in antecedents.keys()
+    }
+
+    if consequent_factors is None:
+        factors = {
+            c: consequent_eq_neq(
+                support=supports[c],
+                antecedents=antecedents.keys(),
+                scale=consequent_scale,
+            )
+            for c in consequents.keys()
+        }
+    else:
+        factors = {c: consequent_factors[c] for c in consequents.keys()}
+
+    if witnesses is None:
+        witness_vars = set(supports.keys()) - set(consequents.keys())
+    else:
+        witness_vars = set(witnesses.keys())
+
+    if witness_preemptions is None:
+        preemptions = {
+            w: undo_split(supports[w], antecedents=antecedents.keys())
+            for w in witness_vars
+        }
+    else:
+        preemptions = {w: witness_preemptions[w] for w in witness_vars}
+
+    antecedent_handler = SplitSubsets(
+        {a: supports[a] for a in antecedents.keys()},
+        interventions,
+        bias=antecedent_bias,
+        prefix=f"{prefix}__antecedent_",
+    )
+    consequent_handler = Factors(factors, prefix=f"{prefix}__consequent_")
+    witness_handler = Preemptions(
+        preemptions, bias=witness_bias, prefix=f"{prefix}__witness_"
+    )
+    with antecedent_handler, witness_handler, consequent_handler:
+        evidence: Mapping[str, Union[Observation[S], Observation[T]]] = {
+            **{a: aa for a, aa in antecedents.items() if aa is not None},
+            **{c: cc for c, cc in consequents.items() if cc is not None},
+            **{w: ww for w, ww in (witnesses or {}).items() if ww is not None},
+        }
+        yield evidence
