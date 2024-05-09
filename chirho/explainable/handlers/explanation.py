1afd6d968e60d0ab092167d98aeb97d7a2546952
Eli <eli@elibingham.com>
2024-05-09 11:02:45 -0400

    docstring

--- chirho/explainable/handlers/explanation.py
+++ chirho/explainable/handlers/explanation.py
@@ -74,7 +74,15 @@ def SearchForExplanation(
     On each run, nodes listed in ``actions`` are randomly selected and intervened on with probability ``.5 + bias``
     (that is, preempted with probability ``.5-bias``). The sampling is achieved by adding stochastic binary preemption
     nodes associated with intervention candidates. If a given preemption node has value ``0``, the corresponding
-    intervention is executed. See tests in `tests/explainable/test_handlers_explanation.py` for examples.
+    intervention is executed.
+
+    When used as a context manager, ``SearchForExplanation`` yields a dictionary of observations
+    that can be used with ``condition`` to simultaneously impose an additional factivity constraint
+    alongside the necessity and sufficiency constraints it implements::
+
+        with SearchForExplanation(...) as evidence:
+            with condition(data=evidence):
+                model()
 
     :param supports: A mapping of sites to their support constraints.
     :param antecedents: A mapping of antecedent names to observations.
