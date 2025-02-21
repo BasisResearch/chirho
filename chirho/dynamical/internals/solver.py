3db516bc503c19331ebd1f84e62504591c1aee3c
rfl-urbaniak <rfl.urbaniak@gmail.com>
2025-02-21 13:18:32 -0500

    add assertion to solver.py to make mypy happy

--- chirho/dynamical/internals/solver.py
+++ chirho/dynamical/internals/solver.py
@@ -66,6 +66,10 @@ class Solver(Generic[T], pyro.poutine.messenger.Messenger):
         start_time: R = msg["args"][2]
         end_time: R = msg["args"][3]
 
+        # needed to avoid mypy error
+        assert isinstance(start_time, torch.Tensor)
+        assert isinstance(end_time, torch.Tensor)
+
         if pyro.settings.get("validate_dynamics"):
             check_dynamics(dynamics, state, start_time, end_time, **msg["kwargs"])
 
