65868cde4a2959ae665a7893b4bc518e6854957f
rfl-urbaniak <rfl.urbaniak@gmail.com>
2025-09-12 12:17:47 -0400

    cleanup and refactoring

--- chirho/observational/ops.py
+++ chirho/observational/ops.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 
 import functools
+from numbers import Number
 from typing import Callable, Hashable, Mapping, Optional, TypeVar, Union
+
+import pyro.distributions as dist
+import torch
+from pyro.distributions.torch_distribution import TorchDistribution
+from torch.distributions import constraints
+from torch.distributions.utils import broadcast_all
 
 T = TypeVar("T")
 
@@ -18,3 +25,206 @@ def observe(rv, obs: Optional[Observation[T]] = None, **kwargs) -> T:
     Observe a random value in a probabilistic program.
     """
     raise NotImplementedError(f"observe not implemented for type {type(rv)}")
+
+
+class ExcisedNormal(TorchDistribution):
+    arg_constraints = {"loc": constraints.real, "scale": constraints.positive}
+    support = (
+        constraints.real
+    )  # we don't want to use intervals here, they might differ between factual points
+    has_rsample = True
+    _mean_carrier_measure = 0
+
+    @property
+    def mean(self):
+        return self.loc
+
+    @property
+    def stddev(self):
+        return self.scale
+
+    @property
+    def variance(self):
+        return self.stddev.pow(2)
+
+    @property
+    def intervals(self):
+        return self._intervals
+
+    @property
+    def base_normal(self):
+        return self._base_normal
+
+    @property
+    def self_base_uniform(self):
+        return self._base_uniform
+
+    @property
+    def interval_masses(self):
+        return self._interval_masses
+
+    @property
+    def lcdfs(self):
+        return self._lcdfs
+
+    @property
+    def removed_pr_mass(self):
+        return self._removed_pr_mass
+
+    @property
+    def normalization_constant(self):
+        return self._normalization_constant
+
+    def __init__(self, loc, scale, intervals, validate_args=None):
+
+        lows, highs = zip(*intervals)  # each is a tuple of tensors/scalars
+
+        self.loc, self.scale, *all_edges = broadcast_all(loc, scale, *lows, *highs)
+
+        n = len(lows)
+        lows = all_edges[:n]
+        highs = all_edges[n:]
+        self._intervals = list(zip(lows, highs))
+
+        if isinstance(loc, Number) and isinstance(scale, Number):
+            batch_shape = torch.Size()
+        else:
+            batch_shape = self.loc.size()
+        super(ExcisedNormal, self).__init__(batch_shape, validate_args=validate_args)
+
+        self._base_normal = dist.Normal(
+            self.loc, self.scale, validate_args=validate_args
+        )
+
+        self._base_uniform = dist.Uniform(
+            torch.zeros_like(self.loc), torch.ones_like(self.loc)
+        )
+
+        # these do not vary and do not depend on sample shape, can be pre-computed
+        self._interval_masses = []
+        self._lcdfs = []
+        self._removed_pr_mass = torch.zeros_like(self.loc)
+
+        for low, high in self.intervals:
+            lower_cdf = self.base_normal.cdf(low)
+            upper_cdf = self.base_normal.cdf(high)
+            interval_mass = upper_cdf - lower_cdf
+            self._interval_masses.append(interval_mass)
+            self._lcdfs.append(lower_cdf)
+            self._removed_pr_mass += interval_mass
+
+        self._normalization_constant = torch.ones_like(self.loc) - self._removed_pr_mass
+
+        lcdfs = torch.stack(self._lcdfs)
+        if not torch.all(lcdfs[:-1] < lcdfs[1:]).item():
+            raise ValueError("lcdfs must be strictly increasing (sorted).")
+
+        for i in range(len(self._intervals) - 1):
+            _, high_i = self._intervals[i]
+            low_next, _ = self._intervals[i + 1]
+            if not torch.all(high_i < low_next).item():
+                raise ValueError("Intervals must not overlap.")
+
+    def expand(self, batch_shape, _instance=None):
+        new = self._get_checked_instance(ExcisedNormal, _instance)
+        batch_shape = torch.Size(batch_shape)
+        new.loc = self.loc.expand(batch_shape)
+        new.scale = self.scale.expand(batch_shape)
+
+        new._intervals = [
+            (low.expand(batch_shape), high.expand(batch_shape))
+            for low, high in self._intervals
+        ]
+        new._base_normal = dist.Normal(new.loc, new.scale, validate_args=False)
+        new._base_uniform = dist.Uniform(
+            torch.zeros_like(new.loc), torch.ones_like(new.loc)
+        )
+
+        new._interval_masses = [im.expand(batch_shape) for im in self.interval_masses]
+        new._lcdfs = [lcdf.expand(batch_shape) for lcdf in self.lcdfs]
+
+        new._removed_pr_mass = self.removed_pr_mass.expand(batch_shape)
+        new._normalization_constant = self.normalization_constant.expand(batch_shape)
+
+        super(ExcisedNormal, new).__init__(batch_shape, validate_args=False)
+        new._validate_args = self._validate_args
+        return new
+
+    def log_prob(self, value):
+
+        shape = value.shape
+
+        mask = torch.zeros(shape, dtype=torch.bool, device=self.loc.device)
+
+        for interval in self.intervals:
+            low, high = interval
+            mask = mask | ((value >= low) & (value <= high))
+
+        normalization_constant_expanded = self.normalization_constant.expand(shape)
+
+        lp = self.base_normal.log_prob(value) - torch.log(
+            normalization_constant_expanded
+        )
+
+        return torch.where(
+            mask, torch.tensor(-float("inf"), device=self.loc.device), lp
+        )
+
+    def cdf(self, value):
+        if self._validate_args:
+            self._validate_sample(value)
+
+        base_cdf = self.base_normal.cdf(value)
+        adjusted_cdf = base_cdf.clone()
+
+        for l_cdf, mass in zip(self.lcdfs, self.interval_masses):
+            adjusted_cdf = torch.where(
+                base_cdf >= l_cdf,
+                adjusted_cdf - torch.clamp(base_cdf - l_cdf, max=mass),
+                adjusted_cdf,
+            )
+
+        adjusted_cdf = adjusted_cdf / self.normalization_constant
+
+        return adjusted_cdf
+
+    def icdf(self, value):
+        if self._validate_args:
+            self._validate_sample(value)
+
+        normalization_constant_expanded = self.normalization_constant.expand(
+            value.shape
+        )
+
+        v = value * normalization_constant_expanded
+
+        for l_cdf, mass in zip(self.lcdfs, self.interval_masses):
+            v = torch.where(v >= l_cdf, v + mass, v)
+
+        x = self.base_normal.icdf(v)
+
+        return x
+
+    def sample(self, sample_shape=torch.Size()):
+
+        with torch.no_grad():
+            uniform_sample = self._base_uniform.sample(sample_shape=sample_shape).to(
+                self.loc.device
+            )
+            x_icdf = self.icdf(uniform_sample)
+
+        return x_icdf
+
+    def rsample(self, sample_shape=torch.Size()):
+
+        # we do not use the reparameterization trick here, but we want gradients to flow to loc and scale
+        # we also don't expect them to flow in excised intervals
+        # but also we don't expect observations in excised intervals either
+
+        uniform_sample = self._base_uniform.sample(sample_shape=sample_shape).to(
+            self.loc.device
+        )
+        uniform_sample.requires_grad_(True)
+        x_icdf = self.icdf(uniform_sample)
+
+        return x_icdf
---
+++ tests/observational/test_excised.py
@@ -0,0 +1,145 @@
+import pytest
+import torch
+
+from chirho.observational.ops import ExcisedNormal
+
+
+# needed for testing interval CDFs
+class ECDF(torch.nn.Module):
+    def __init__(self, x, side="right"):
+        super(ECDF, self).__init__()
+
+        if side.lower() not in ["right", "left"]:
+            msg = "side can take the values 'right' or 'left'"
+            raise ValueError(msg)
+        self.side = side
+
+        if len(x.shape) != 1:
+            msg = "x must be 1-dimensional"
+            raise ValueError(msg)
+
+        x = x.sort()[0]
+        nobs = len(x)
+        y = torch.linspace(1.0 / nobs, 1, nobs, device=x.device)
+
+        self.x = torch.cat((torch.tensor([-torch.inf], device=x.device), x))
+        self.y = torch.cat((torch.tensor([0], device=y.device), y))
+        self.n = self.x.shape[0]
+
+    def forward(self, time):
+        tind = torch.searchsorted(self.x, time, side=self.side) - 1
+        return self.y[tind]
+
+
+@pytest.fixture
+def true_parameters():
+    mean = torch.tensor([[[1.0]], [[3.0]]], requires_grad=True)
+    stddev = torch.tensor([[[2.0]], [[3.0]]], requires_grad=True)
+    return mean, stddev
+
+
+@pytest.mark.parametrize(
+    "interval_key, intervals",
+    [
+        ("flat", [(torch.tensor(-1.0), torch.tensor(1.0))]),
+        ("flat_intervals", [(-2.0, -1.0), (1.0, 2.0)]),
+        ("shaped", None),  # will fill in fixture
+        ("shaped_intervals", None),
+    ],
+)
+def test_excised_normal_shapes_and_sampling(true_parameters, interval_key, intervals):
+    mean, stddev = true_parameters
+
+    shaped_intervals = [(mean - stddev, mean + stddev)]
+    shaped_intervals_multiple = [
+        (mean - 2 * stddev, mean - stddev),
+        (mean + stddev, mean + 2 * stddev),
+    ]
+
+    if interval_key == "shaped":
+        intervals = shaped_intervals
+    elif interval_key == "shaped_intervals":
+        intervals = shaped_intervals_multiple
+
+    excised_normal = ExcisedNormal(mean, stddev, intervals)
+
+    new_batch_shape = (2, 1, 3)
+    excised_normal_expanded = excised_normal.expand(new_batch_shape)
+
+    # --- Basic shape checks ---
+    for dist_obj in [excised_normal, excised_normal_expanded]:
+        assert dist_obj.mean.shape == dist_obj.loc.shape
+        assert dist_obj.stddev.shape == dist_obj.scale.shape
+        assert dist_obj.variance.shape == dist_obj.scale.shape
+        for low, high in dist_obj._intervals:
+            assert low.shape == dist_obj.loc.shape
+            assert high.shape == dist_obj.loc.shape
+
+    # --- Interval masses as expected ---
+    if interval_key == "shaped":
+        for (
+            lcdf
+        ) in excised_normal.lcdfs:  # these are expected to be *base* normal lcdfs
+            assert torch.allclose(
+                lcdf, torch.tensor(0.1587), atol=1e-4
+            )  # below mean - 1 stddev
+        for im in excised_normal.interval_masses:
+            assert torch.allclose(
+                im, torch.tensor(0.6827), atol=1e-4
+            )  # between mean - 1 stddev and mean + 1 stddev
+
+    # --- Sampling ---
+    sample = excised_normal.sample(sample_shape=(400,))
+    sample_expanded = excised_normal_expanded.sample(sample_shape=(400,))
+
+    assert sample.shape == (400, 2, 1, 1)
+    assert sample_expanded.shape == (400, 2, 1, 3)
+
+    # --- samples avoid intervals ---
+    for low, high in excised_normal._intervals:
+        assert torch.all((sample <= low) | (sample >= high))
+    for low, high in excised_normal_expanded._intervals:
+        assert torch.all((sample_expanded <= low) | (sample_expanded >= high))
+
+    # --- Log probability checks ---
+    candidates = (torch.rand(sample.shape) - 0.5) * 40
+    log_probs = excised_normal.log_prob(candidates)
+    mask = torch.zeros_like(candidates, dtype=torch.bool)
+    for low, high in excised_normal._intervals:
+        mask |= (candidates >= low) & (candidates <= high)
+    assert torch.all(torch.where(mask, log_probs == -float("inf"), True))
+
+    # --- CDF vs ECDF  ---
+    ecdf_sample_module = ECDF(sample[:, 0, 0, 0])
+    ecdf_sample = ecdf_sample_module(sample[:, 0, 0, 0])
+    cdf_sample = excised_normal.cdf(sample)[:, 0, 0, 0].detach()
+    assert torch.allclose(ecdf_sample, cdf_sample, atol=0.1)
+
+    ecdf_sample_exp_module = ECDF(sample_expanded[:, 0, 0, 0])
+    ecdf_sample_exp = ecdf_sample_exp_module(sample_expanded[:, 0, 0, 0])
+    cdf_sample_exp = excised_normal_expanded.cdf(sample_expanded)[:, 0, 0, 0].detach()
+    assert torch.allclose(ecdf_sample_exp, cdf_sample_exp, atol=0.1)
+
+    # --- rsample and backpropagation ---
+    cloned_mean = mean.detach().clone().requires_grad_(True)
+    cloned_stddev = stddev.detach().clone().requires_grad_(True)
+    new_interval = [(torch.tensor(-1.0), torch.tensor(1.0))]
+    excised_normal_copy = ExcisedNormal(cloned_mean, cloned_stddev, new_interval)
+    r_sample = excised_normal_copy.rsample(sample_shape=(400,))
+    loss = ((r_sample - 1.0) ** 2).mean()
+    optimizer = torch.optim.SGD(
+        [excised_normal_copy.loc, excised_normal_copy.scale], lr=0.1
+    )
+    loc_before, scale_before = (
+        excised_normal_copy.loc.clone(),
+        excised_normal_copy.scale.clone(),
+    )
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    loc_after, scale_after = (
+        excised_normal_copy.loc.clone(),
+        excised_normal_copy.scale.clone(),
+    )
+    assert not torch.allclose(loc_before, loc_after)
+    assert not torch.allclose(scale_before, scale_after)
